eeb801e92058657571c7ee11b88b8e8acdf1a06c
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-05 08:00:52 +0300

    Time: 0 ms (100%), Space: 19.3 MB (17.17%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -5,25 +5,19 @@ class Solution:
         for token in tokens : 
             if token == "+": 
                 a, b, = stack.pop(), stack.pop()
-                print(f"adding {a} and {b}")
                 stack.append(b+a)
             elif token == "/": 
                 a, b, = stack.pop(), stack.pop()
-                print(f"dividing {b} by {a}")
                 temp = b/a
                 if temp < 0 : 
-                    print('==== found -ve number ====')
                     stack.append( math.ceil(temp))
                 else : 
                      stack.append(math.floor(temp))
-                   
             elif token == "*": 
-                a, b, = stack.pop(), stack.pop()
-                print(f"mult {a} and {b}")
+                a, b, = stack.pop(), stack.pop()  
                 stack.append(b*a)
             elif token == "-": 
                 a, b, = stack.pop(), stack.pop()
-                print(f"subtracting {a} from {b}")
                 stack.append(b-a)
             else : 
                 stack.append(int(token))
