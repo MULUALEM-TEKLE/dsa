eebd0fc2d2afc78e586e4b4281e637b99b5ea673
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-06 13:55:41 +0300

    Time: 63 ms (70.7%), Space: 23.5 MB (11.56%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -3,17 +3,17 @@ class Solution:
         sqrt_list = [(i[0]**2 + i[1]**2) for i in points]
         table = {}
 
-        for i, sqrt in enumerate(sqrt_list):
-            if sqrt in table.keys():
-                table[sqrt].append(i)
+        for i in range(len(sqrt_list)):
+            if sqrt_list[i] in table:
+                table[sqrt_list[i]].append(i)
             else:
-                table[sqrt] = [i]
+                table[sqrt_list[i]] = [i]
          
         finalists = sorted(table.keys())[:k]
        
 
         temp = []
-        for i in range(k): 
+        for i in range(len(finalists)): 
             if not len(temp) >= k:
                 temp.extend(table[finalists[i]])
                           
