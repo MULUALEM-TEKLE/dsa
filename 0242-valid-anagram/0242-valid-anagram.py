40445ecac52cfb2ac4c1251a99c92338526f6690
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-09 11:43:50 +0300

    Time: 15 ms (42.82%), Space: 18.5 MB (19.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,38 +5,38 @@ class Solution:
         if len(s) != len(t) : 
             return False
         # approach one
-        # else construct two tables with word frequencies and add letters as keys with values of frequencies
-        # compare for a letter frequencies and if all are equal return true, if not return false
-        s_table = {}
-        t_table = {}
+        # else construct two tables with word frequencies 
+        # and add letters as keys with values of frequencies
+        # compare for a letter frequencies and if all are equal return true, 
+        # if not return false
+        # s_table = {}
+        # t_table = {}
 
-        for letter in s :
-            if letter in s_table.keys() : 
-                s_table[letter] += 1
-            else : 
-                s_table[letter] = 1
+        # for letter in s :
+        #     if letter in s_table.keys() : 
+        #         s_table[letter] += 1
+        #     else : 
+        #         s_table[letter] = 1
         
-        for letter in t :
-            if letter not in s_table.keys() : 
-                return False
+        # for letter in t :
+        #     # if t has a letter that is not in s we're done =)
+        #     if letter not in s_table.keys() : 
+        #         return False
 
-            if letter in t_table.keys() : 
-                t_table[letter] += 1
-            else : 
-                t_table[letter] = 1
+        #     if letter in t_table.keys() : 
+        #         t_table[letter] += 1
+        #     else : 
+        #         t_table[letter] = 1
 
-        for letter in s :    
-            if s_table[letter] != t_table[letter]:
-                return False
+        # for letter in s :    
+        #     if s_table[letter] != t_table[letter]:
+        #         return False
         
-        return True
+        # return True
 
-
-
-
-        # approach 4
+        # approach two (lazy)
         # just sort the compare
         # t = sorted(t)
         # s = sorted(s)
-        # return t == s
+        return sorted(t) == sorted(s)
         
