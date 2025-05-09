b0bfb52a4274ff3686bb17667184000eab856dff
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-09 13:33:14 +0300

    Time: 4 ms (12.67%), Space: 17.8 MB (50.47%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -3,14 +3,15 @@ class Solution:
         if num == 1 : return True 
         test = num // 2
         while True: 
-            print(test)
             if test * test > num : 
                 test = test//2
                 continue
-            elif test * test == num : 
-                return True
-            else : 
-                break
+            break
+                # continue
+            # elif test * test == num : 
+            #     return True
+            # else : 
+            #     break
         
         for i in range(test, num) : 
             if i * i > num : 
