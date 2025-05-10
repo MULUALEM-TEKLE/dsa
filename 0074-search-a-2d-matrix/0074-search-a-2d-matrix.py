dcd002f82f8667d6dfe18d72746c0b60d97b6594
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-10 16:32:24 +0300

    Time: 0 ms (100%), Space: 18.3 MB (45.42%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,43 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # first lets find on which row the target might reside in
+        low , high = 0 , len(matrix) -1
+        mid = None
+        while low <= high:
+            mid = (low + high)//2
+            print(f"mid is now {mid}")
+            if target > matrix[mid][0] : 
+                print(f"since target > {matrix[mid][0]} ")
+                low = mid + 1
+                print(f"low is now {low}")
+            elif target < matrix[mid][0] : 
+                print(f"since target < {matrix[mid][0]} ")
+                high = mid -1
+                print(f"high is now {high}")
+            else : 
+               return True
+
+
+        # print(f"location is {mid}")
+        # print(f"low is {low}")
+        # print(f"high is {high}")
+        location = matrix[high]
+        low , high = 0 , len(location) -1
+        while low <= high:
+            mid = (low + high)//2
+            print(f"mid is now {mid}")
+            if target > location[mid] : 
+                low = mid + 1
+            elif target < location[mid] : 
+                high = mid -1
+            else : 
+               return True
+               
+        return False
+       
+
+        # low , high = 0 , len(matrix[location]) -1
+
+
+        # after zooming in like that we might search inside the specific row 
+        
