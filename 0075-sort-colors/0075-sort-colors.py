8d7da2b3edd24b8d6d38b58746046f59d4268638
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-10 11:57:13 +0300

    Time: 0 ms (100%), Space: 17.9 MB (33.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -6,11 +6,17 @@ class Solution:
         for num in nums :
             counter[num] += 1
         print(counter)
-        nums.clear()
-        for i in range(len(counter)):
-            nums.extend([i] * counter[i])
+        # nums.clear()
+        # for i in range(len(counter)):
+        #     nums.extend([i] * counter[i])
         
-        print(nums)
+        # print(nums)
+        i = 0
+        for n in range(len(counter)):
+            for _ in range(counter[n]) : 
+                nums[i] = n
+                i +=1
+        
         """
         Do not return anything, modify nums in-place instead.
         """
