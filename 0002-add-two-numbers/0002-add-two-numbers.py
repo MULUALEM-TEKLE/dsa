94d7cdb2e1ba6497e16e9b6b028b876eca7f087c
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-13 07:51:48 +0300

    Time: 4 ms (55.2%), Space: 17.8 MB (56.68%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,50 +5,25 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry = 0
+        total =carry = 0
         result = ListNode()
         cur = result
 
-        while l1 and l2 : 
-            sum_ = l1.val + l2.val + carry
-            print(f"sum is {sum_}")
-            if sum_ > 9 : 
-                carry = 1
-                cur.next = ListNode(sum_ % 10)
-            else : 
-                carry = 0
-                cur.next = ListNode(sum_)
-            l1 = l1.next
-            l2 = l2.next
-            cur = cur.next
-        
-        while l1 : 
-            sum_ = l1.val + carry
-            print(f"sum is {sum_}")
-            if sum_ > 9 : 
-                carry = 1
-                cur.next = ListNode(sum_ % 10)
-            else : 
-                carry = 0
-                cur.next = ListNode(sum_)
-            l1 = l1.next
-            cur = cur.next
+        while l1 or l2 or carry : 
+            total = carry
 
-        while l2 : 
-            sum_ = l2.val + carry
-            print(f"sum is {sum_}")
-            if sum_ > 9 : 
-                carry = 1
-                cur.next = ListNode(sum_ % 10)
-            else : 
-                carry = 0
-                cur.next = ListNode(sum_)
-            l2 = l2.next
+            if l1 : 
+                total += l1.val
+                l1 = l1.next
+            if l2 :
+                total += l2.val
+                l2 = l2.next
+            
+            num = total % 10
+            carry = total // 10
+            cur.next = ListNode(num)
             cur = cur.next
         
-        if carry : 
-            cur.next = ListNode(1)
-            
         
         return result.next
         
