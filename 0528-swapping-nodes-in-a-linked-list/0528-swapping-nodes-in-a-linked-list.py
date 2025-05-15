e3330b683ba0a6decada3f8b0213e7e10490a61b
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-15 09:16:19 +0300

    Time: 48 ms (39.34%), Space: 40.1 MB (70.13%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
@@ -5,18 +5,35 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        array = []
-        cur = head
-        while cur :
-            array.append(cur.val)
+        # array = []
+        # cur = head
+        # while cur :
+        #     array.append(cur.val)
+        #     cur = cur.next
+        
+        # array[k -1], array[-k] = array[-k] , array[k -1]
+
+        # dummy = ListNode()
+        # cur = dummy 
+        # for num in array:
+        #     cur.next = ListNode(num)
+        #     cur = cur.next
+
+        # return dummy.next
+
+        cur = head 
+        for i in range(k-1) : 
             cur = cur.next
         
-        array[k -1], array[-k] = array[-k] , array[k -1]
+        print(f"cur is at {cur.val}")
 
-        dummy = ListNode()
-        cur = dummy 
-        for num in array:
-            cur.next = ListNode(num)
-            cur = cur.next
+        right = head 
+        left = cur 
 
-        return dummy.next
+        while cur.next : 
+            cur = cur.next 
+            right = right.next 
+
+        right.val , left.val = left.val, right.val
+        return head
+        # print(f"finally left ia at {left.val} and right is at {right.val}")
