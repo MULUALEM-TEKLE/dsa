1d7fb34a35bcb5000500e3cd792fbe5ed41a2f15
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-17 11:58:54 +0300

    Time: 11 ms (82.95%), Space: 20.6 MB (76.54%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        table = {}
+
+        for index, word in enumerate(["".join(sorted(s)) for s in strs]) : 
+            if word in table.keys() : 
+                table[word].append(strs[index])
+            else : 
+                table[word] = [strs[index]]
+
+        return list(table.values())
