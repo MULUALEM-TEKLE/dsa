d5d4e1edd4bf6d86b915e4003a7be80a6a9d036c
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-19 11:18:38 +0300

    Time: 39 ms (40.23%), Space: 34.8 MB (62.04%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -3,14 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-def print_list(head): 
-    s = ""
-    while head : 
-        s += f"{head.val} ->"
-        head = head.next
-    print(s[:-2])
-
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         fast = slow = head
@@ -20,19 +12,18 @@ class Solution:
             slow = slow.next
         
         second_half = None
-        cur = slow
 
-        while cur : 
-            next_node = cur.next
-            cur.next = second_half
-            second_half = cur
-            cur = next_node
+        while slow : 
+            next_node = slow.next
+            slow.next = second_half
+            second_half = slow
+            slow = next_node
 
-        first_half = head
+        
         while second_half : 
-            if first_half.val != second_half.val : 
+            if head.val != second_half.val : 
                 return False
-            first_half = first_half.next
+            head = head.next
             second_half = second_half.next
 
         return True
