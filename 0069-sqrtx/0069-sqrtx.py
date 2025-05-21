b79998359d3bebc40cb136558e5903958d9856bc
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-21 16:07:07 +0300

    Time: 1 ms (69.87%), Space: 17.6 MB (81.23%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left , right = 0 , x 
+        res = 0
+
+        while left <= right : 
+            m = (left + right )//2
+            if m ** 2 > x : 
+                right = m -1 
+            elif m ** 2 < x : 
+                left = m + 1 
+                res = m 
+            else : 
+                return m
+
+        return res
+           
+    
