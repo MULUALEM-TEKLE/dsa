378629817040a0889f3c762cf1b8489ac70544b5
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-21 16:13:07 +0300

    Time: 11 ms (17.95%), Space: 19.9 MB (9.67%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        table = {}
+        for num in nums : 
+            if num in table.keys() : 
+                table[num] += 1
+            else : 
+                table[num] = 1
+
+        for key, value in table.items() : 
+            if value == 1 : 
+                return key
