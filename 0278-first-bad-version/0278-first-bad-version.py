ef4a247d8a0007a6d88ac24781cd05ad45474479
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-25 22:55:48 +0300

    Time: 46 ms (5.41%), Space: 17.9 MB (26.63%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,7 +3,7 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        low , high = 0 , n
+        low , high = 1 , n
 
         while low < high : 
             mid = low + ((high - low)//2)
