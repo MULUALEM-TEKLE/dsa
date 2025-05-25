e151712afc9faf63f07a98258dc484a29ee5e431
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-25 16:38:51 +0300

    Time: 59 ms (50.16%), Space: 17.9 MB (61.95%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.py
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -1,13 +1,11 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         result = []
-        left = 0
         for i , right in enumerate(range(n, len(nums))):
             if i == n : 
                 return
             result.append(nums[i])
             result.append(nums[right])
-            left += 1
         return result
 
         
