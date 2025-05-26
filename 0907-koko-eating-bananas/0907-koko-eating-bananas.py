5df90b0ea73f12bd1ba32f8ebf0cbdfd45c14c40
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-26 22:20:31 +0300

    Time: 272 ms (5.01%), Space: 19 MB (29.62%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -7,12 +7,10 @@ class Solution:
         low , high = 1 , max(piles)
 
         def totalTimePerRate(rate  ) : 
-          
-
             return sum([math.ceil(pile/rate) for pile in piles])
 
         while low < high : 
-            mid = ((low+high)//2)
+            mid = low + (high - low) // 2
             if totalTimePerRate(mid ) > h : 
                 low = mid +1 
             elif totalTimePerRate(mid ) <= h :
