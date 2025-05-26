b7820915a96013106e8ec54518e60cb7c7783a3e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-26 22:19:21 +0300

    Time: 327 ms (5.01%), Space: 18.9 MB (48.67%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,22 +1,23 @@
 import math
 
-def totalTimePerRate(rate , pile ) : 
-    total_time = 0
-    
-    for i in range(len(pile)) : 
-        total_time += math.ceil(pile[i] / rate)
-
-    return total_time
     
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         low , high = 1 , max(piles)
 
+        def totalTimePerRate(rate  ) : 
+            total_time = 0
+            
+            for i in range(len(piles)) : 
+                total_time += math.ceil(piles[i] / rate)
+
+            return total_time
+
         while low < high : 
             mid = ((low+high)//2)
-            if totalTimePerRate(mid , piles[:]) > h : 
+            if totalTimePerRate(mid ) > h : 
                 low = mid +1 
-            elif totalTimePerRate(mid , piles[:]) <= h :
+            elif totalTimePerRate(mid ) <= h :
                 high = mid 
         return low
