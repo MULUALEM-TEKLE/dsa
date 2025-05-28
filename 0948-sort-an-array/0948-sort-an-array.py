1bd858a2cb25cf434d937a5b606ff647290ca0cd
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-28 11:39:16 +0300

    Time: 714 ms (34.77%), Space: 24.2 MB (87.37%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,44 +1,43 @@
-def merge(nums , start , mid ,  end ) : 
-    L = nums[start : mid + 1]
-    R = nums[mid + 1  : end + 1]
-
-    LP = RP = 0 
-    NP = start
-
+def merge(nums , start , mid , end) : 
+    # copy left and right arrays
+    L = nums[start : mid + 1 ]
+    R = nums[mid + 1 : end + 1 ]
+    # instantiate pointers
+    LP = RP = 0
+    AP = start
+    # compare and merge from left and right
     while LP < len(L) and RP < len(R) : 
-        if L[LP] <= R[RP] : 
-            nums[NP] = L[LP]
+        if L[LP] < R[RP] : 
+            nums[AP] = L[LP]
             LP += 1 
         else : 
-            nums[NP] = R[RP]
-            RP += 1
-        NP += 1 
-    
+            nums[AP] = R[RP]
+            RP += 1 
+        AP += 1
+    # merge what is left from the right and left arrays
     while LP < len(L) : 
-        nums[NP] = L[LP]
+        nums[AP] = L[LP]
         LP += 1 
-        NP += 1 
+        AP += 1
 
     while RP < len(R) : 
-        nums[NP] = R[RP]
+        nums[AP] = R[RP]
         RP += 1 
-        NP += 1 
-    
+        AP += 1
 
 class Solution:
-    def sort_helper(self , nums , start , end) : 
-        if start >= end : 
-            return 
-
-        mid = (start + end) //2
-
+    def sort_helper(self ,nums , start , end) : 
+        # define base conditions
+        if start >= end :
+            return  
+        # find the mid 
+        mid = start + (end - start)//2
+        # do the recursive calls
         self.sort_helper(nums , start , mid)
-        self.sort_helper(nums , mid + 1 , end)
-
+        self.sort_helper(nums ,mid + 1 , end)
+        # merge left and right halves
         merge(nums , start , mid , end)
 
     def sortArray(self, nums: List[int]) -> List[int]:
-
         self.sort_helper(nums , 0 , len(nums) -1 )
         return nums
-
