107ab0c4e900dcdb5dd8acf5559da819c31aac3c
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-29 17:55:03 +0300

    Time: 3 ms (54.54%), Space: 17.9 MB (23.45%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        t_table = defaultdict(int)
+        s_table = defaultdict(int)
+
+        for letter in s : 
+            s_table[letter] += 1 
+
+        for letter in t : 
+            t_table[letter] += 1 
+
+        for letter in t : 
+            if t_table[letter] != s_table[letter] : 
+                return letter
