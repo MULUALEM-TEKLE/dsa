ac57e7231eeea5e5f83b57f56f60a3c028a176f3
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-29 17:44:10 +0300

    Time: 39 ms (6.66%), Space: 17.7 MB (96.48%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazine_freq = {}
+        ransom_freq = {}
+
+        for letter in magazine : 
+            if letter in magazine_freq.keys() : 
+                magazine_freq[letter] += 1 
+            else : 
+                magazine_freq[letter] = 1
+
+        for letter in ransomNote : 
+            if letter in ransom_freq.keys() : 
+                ransom_freq[letter] += 1 
+            else : 
+                ransom_freq[letter] = 1
+        
+        for letter in ransomNote : 
+            if letter not in magazine_freq.keys() or ransom_freq[letter] > magazine_freq[letter] : 
+                return False
+
+        return True
+        
