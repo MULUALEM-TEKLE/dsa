e3780830c31207214054917a7250181646599514
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-05-29 12:29:39 +0300

    Time: 0 ms (100%), Space: 18 MB (14.75%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
@@ -1,7 +1,24 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        for i in range(1 , max(arr) + k + 1 ) : 
-            if i not in arr : 
-                k -= 1 
-                if k == 0 : 
-                    return i
+        # for i in range(1 , max(arr) + k + 1 ) : 
+        #     if i not in arr and  : 
+        #         k -= 1 
+        #         if k == 0 : 
+        #             return i
+
+        low , high = 0 , len(arr) 
+
+        while low < high : 
+            mid = (low + high)//2
+
+            if (arr[mid] - mid - 1) < k : 
+                low = mid + 1 
+            else : 
+                high = mid
+        
+        return low + k
+
+'''
+[2,3,4,7,11]
+ 0 1 2 3  4
+'''
