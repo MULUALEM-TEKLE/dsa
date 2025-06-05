876f5608a96a8aebf7b0e155b71dbe5830809d51
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-05 18:40:20 +0300

    Time: 3 ms (29.05%), Space: 19 MB (42.83%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,15 +6,15 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        # lets do preorder traversal
-        # check if node is null
-        if not root : 
-            return False
-        targetSum -= root.val
-        # check if we're at a leaf node and if target sum is reached return true
-        if not root.right and not root.left and targetSum == 0:
-            return True
-        return self.hasPathSum(root.left , targetSum) or self.hasPathSum(root.right , targetSum)
-        # check left
-        # check right
-        # if nothing found return false
+        def dfs(root , targetSum) : 
+            if not root : 
+                return False
+            
+            targetSum -= root.val
+
+            if not root.left and not root.right : 
+                return targetSum == 0
+            
+            return dfs(root.left , targetSum) or dfs(root.right , targetSum)
+
+        return dfs(root, targetSum)
