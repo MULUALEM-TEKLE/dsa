57bc8903035c8483424ce250c6846237ee277f63
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-14 12:15:18 +0300

    Time: 1 ms (75.38%), Space: 18.1 MB (27.01%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,10 +1,12 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        # counter = {}
+        max_range = max(nums) + 2
+        bucket = [0] * (max_range)
 
-        # for index , num in enumerate(sorted(nums)) : 
-        #     if num not in counter : 
-        #         counter[num] = index
+        for num in nums : 
+            bucket[num+1] += 1 
         
-        # return [counter[num] for num in nums]
-        return [sorted(nums).index(x) for x in nums]
+        for i in range(1 , max_range) : 
+            bucket[i] += bucket[i-1]
+        
+        return [bucket[num] for num in nums]
