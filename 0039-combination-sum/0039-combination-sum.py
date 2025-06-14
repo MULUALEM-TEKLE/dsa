05a40b3ec9c323fef1dfe87f2f8018dc0d08d32d
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-14 11:53:49 +0300

    Time: 3 ms (97.51%), Space: 17.8 MB (61.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
         res = []
 
         subset= []
@@ -10,7 +11,10 @@ class Solution:
             if cur_sum == target : 
                 res.append(subset[:])
                 return 
-            
+
+            if cur_sum + candidates[i] > target :
+                return
+
             subset.append(candidates[i])
             dfs(i , candidates[i] + cur_sum)
             subset.pop()
