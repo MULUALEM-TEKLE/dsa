622a4880dc29b427beb99e743f0bbc1e3d040a13
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-14 11:50:19 +0300

    Time: 11 ms (58.24%), Space: 18 MB (39.68%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -14,8 +14,6 @@ class Solution:
             subset.append(candidates[i])
             dfs(i , candidates[i] + cur_sum)
             subset.pop()
-            while i + 1 < len(candidates) and candidates[i]==candidates[i+1] : 
-                i += 1 
             dfs(i+1 ,cur_sum)
         
         dfs(0 , 0)
