0c35fef269f2b4922ecfdc3c6f7107c8d5555074
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-14 11:21:33 +0300

    Time: 732 ms (27.68%), Space: 24.3 MB (74.45%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,43 +1,39 @@
-def merge(nums , start , mid , end) : 
-    # copy left and right arrays
-    L = nums[start : mid + 1 ]
-    R = nums[mid + 1 : end + 1 ]
-    # instantiate pointers
+def merge(arr ,start , mid , end) : 
+    L = arr[start : mid + 1]
+    R = arr[mid + 1 : end + 1]
     LP = RP = 0
-    AP = start
-    # compare and merge from left and right
-    while LP < len(L) and RP < len(R) : 
-        if L[LP] < R[RP] : 
-            nums[AP] = L[LP]
-            LP += 1 
-        else : 
-            nums[AP] = R[RP]
-            RP += 1 
-        AP += 1
-    # merge what is left from the right and left arrays
-    while LP < len(L) : 
-        nums[AP] = L[LP]
-        LP += 1 
-        AP += 1
+    AP = start 
 
+    while LP < len(L) and RP < len(R) : 
+        if R[RP] < L[LP] : 
+            arr[AP] = R[RP]
+            RP += 1 
+        else : 
+            arr[AP] = L[LP]
+            LP += 1 
+        AP += 1 
+    
+    while LP < len(L) : 
+        arr[AP] = L[LP]
+        LP += 1 
+        AP += 1 
     while RP < len(R) : 
-        nums[AP] = R[RP]
+        arr[AP] = R[RP]
         RP += 1 
-        AP += 1
+        AP += 1 
+    
+def sort(arr , start , end) : 
+    if end - start + 1 <= 1 : 
+        return 
+    
+    mid = (start + end )//2
+
+    sort(arr , start , mid)
+    sort(arr , mid + 1 , end)
+
+    merge(arr , start , mid , end)
 
 class Solution:
-    def sort_helper(self ,nums , start , end) : 
-        # define base conditions
-        if start >= end :
-            return  
-        # find the mid 
-        mid = start + (end - start)//2
-        # do the recursive calls
-        self.sort_helper(nums , start , mid)
-        self.sort_helper(nums ,mid + 1 , end)
-        # merge left and right halves
-        merge(nums , start , mid , end)
-
     def sortArray(self, nums: List[int]) -> List[int]:
-        self.sort_helper(nums , 0 , len(nums) -1 )
+        sort(nums , 0 , len(nums)-1)
         return nums
