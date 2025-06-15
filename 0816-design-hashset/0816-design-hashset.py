7e65462e2b0ae9c258300d566ad0131f119cb888
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-15 09:38:11 +0300

    Time: 87 ms (35.41%), Space: 24.2 MB (30.53%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.py
+++ 0816-design-hashset/0816-design-hashset.py
@@ -1,50 +1,42 @@
+class ListNode :
+    def __init__(self, key=-1 , next=None) : 
+        self.key = key
+        self.next = None  
+
 class MyHashSet:
 
     def __init__(self):
-        self.size = 0
-        self.capacity = 10
-        self.buckets = [[] for _ in range(self.capacity)]
-    
-    def hash(self, key) : 
-        return key % self.capacity
-    
-    def rehash(self) : 
-        self.capacity = 2 * self.capacity 
-        new_buckets = [[] for _ in range(self.capacity)]
+        self.set = [ListNode(0) for _ in range(10**4)]
 
-        old_buckets = self.buckets 
-        self.buckets = new_buckets
-        self.size = 0 
-
-        for chain in old_buckets : 
-            for item in chain : 
-                self.add(item)
 
     def add(self, key: int) -> None:
-        index = self.hash(key)
-        chain = self.buckets[index]
-        if key not in chain : 
-            chain.append(key)
-            self.size += 1
+        cur = self.set[key % len(self.set)]
 
-        if self.size >= self.capacity // 2 : self.rehash()
-                
+        while cur.next : 
+            if cur.next.key == key :
+                return
+            cur = cur.next
+
+        cur.next = ListNode(key) 
+        
 
     def remove(self, key: int) -> None:
-        index = self.hash(key)
-        chain = self.buckets[index]
+        cur = self.set[key % len(self.set)]
 
-        for index , item in enumerate(chain) : 
-            if item == key : del chain[index]
+        while cur.next : 
+            if cur.next.key == key :
+                cur.next = cur.next.next
+                return 
+            cur = cur.next
+
 
     def contains(self, key: int) -> bool:
-        index = self.hash(key)
-        chain = self.buckets[index]
+        cur = self.set[key % len(self.set)]
 
-        # print(self.buckets)
-
-        for item in chain : 
-            if item == key : return True
+        while cur.next : 
+            if cur.next.key == key :
+                return True
+            cur = cur.next
         
         return False
         
