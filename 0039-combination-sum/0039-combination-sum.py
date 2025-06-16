766b92377758523d22285b15d80774e2768febfa
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-16 10:52:07 +0300

    Time: 7 ms (82.9%), Space: 17.7 MB (87.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,22 +3,19 @@ class Solution:
         candidates.sort()
         res = []
 
-        subset= []
+        subset = []
         def dfs(i , cur_sum) : 
-            if cur_sum > target or i >= len(candidates) : 
-                return 
-            
             if cur_sum == target : 
                 res.append(subset[:])
-                return 
 
-            if cur_sum + candidates[i] > target :
+            if i >= len(candidates) or cur_sum > target or cur_sum + candidates[i] > target : 
                 return
 
             subset.append(candidates[i])
-            dfs(i , candidates[i] + cur_sum)
+            dfs(i , cur_sum + candidates[i])
             subset.pop()
-            dfs(i+1 ,cur_sum)
+            dfs(i + 1 , cur_sum)
         
         dfs(0 , 0)
         return res
+        
