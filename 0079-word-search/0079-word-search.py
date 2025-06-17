e118bb93dc263a67a4f9d3f1034d92fdef27dbb6
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-17 12:57:28 +0300

    Time: 7882 ms (5%), Space: 17.9 MB (65.17%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,36 +1,26 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows , cols = len(board) , len(board[0])
-        directions = [[1 , 0] , [-1 , 0] , [0 , 1] , [0 , -1]]
+        rows, cols = len(board) , len(board[0])
+        path = set()
+
+        def dfs(r , c , i) : 
+            if i == len(word) : 
+                return True 
+            
+            if r not in range(rows) or c not in range(cols) or word[i] != board[r][c] or (r , c) in path : 
+                return False
+
+            
+            path.add((r , c))
+            res = dfs(r+1 , c , i+1) or dfs(r-1 , c , i+1) or dfs(r , c+1 , i+1) or dfs(r , c-1 , i+1)
+            path.remove((r ,c))
+            return res
+        
         res = False
 
-        def bfs(r , c) : 
-            stack = []
-            visited = set()
-            stack.append((r , c , 0 , {(r , c)}  ))
-            visited.add((r , c))
-            
-
-            while stack : 
-                row , col , idx , visited  = stack.pop()
-
-                if idx == len(word) - 1 : 
-                    return True
-            
-                for dr,dc in directions : 
-                    r , c = row + dr , col + dc
-                    if r in range(rows) and c in range(cols) and (r , c) not in visited and board[r][c] == word[idx + 1]:   
-                        new_visited = visited.copy()
-                        new_visited.add((r , c))
-                        stack.append((r , c , idx + 1 , new_visited ))
-                       
-            return False
-
-
-        for r in range(rows) : 
+        for r in range(rows): 
             for c in range(cols) : 
-                if board[r][c] == word[0]: 
-                   res = res or bfs(r,c )
-
+                if board[r][c] == word[0] : 
+                    res = res or dfs(r , c , 0)
         
         return res
