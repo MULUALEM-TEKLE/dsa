7bd742c750f6ee96d32d4c11720dd03ccb7a999e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-17 12:39:01 +0300

    Time: 8335 ms (5%), Space: 17.8 MB (87.75%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,35 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows , cols = len(board) , len(board[0])
+        directions = [[1 , 0] , [-1 , 0] , [0 , 1] , [0 , -1]]
+        res = False
+
+        def bfs(r , c) : 
+            stack = []
+            stack.append((r , c , 0 ,{(r , c)}))
+            constructed = board[r][c]
+            
+
+            while stack : 
+                row , col , idx , visited = stack.pop()
+
+                if idx == len(word) - 1 : 
+                    return True
+            
+                for dr,dc in directions : 
+                    r , c = row + dr , col + dc
+                    if r in range(rows) and c in range(cols) and (r , c) not in visited and board[r][c] == word[idx + 1]:
+                        new_visited = visited.copy()
+                        new_visited.add((r , c))
+                        stack.append((r , c , idx + 1 , new_visited))
+                       
+            return False
+
+
+        for r in range(rows) : 
+            for c in range(cols) : 
+                if board[r][c] == word[0]: 
+                   res = res or bfs(r,c )
+
+        
+        return res
