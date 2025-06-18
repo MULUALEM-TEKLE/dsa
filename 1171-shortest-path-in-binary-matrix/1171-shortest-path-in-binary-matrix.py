83459105374c7633aec8bd2ed32a8878cc10a880
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-18 16:47:03 +0300

    Time: 430 ms (5.01%), Space: 19.8 MB (7.03%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,32 +1,35 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         rows , cols = len(grid) , len(grid[0])
-        def bfs(r , c) : 
-            visited = set()
-            q = deque()
-            visited.add((r , c))
-            q.append((r , c , 1))
-            
 
-            while q : 
-                row , col, length = q.popleft()
-
-                if row == rows-1 and col == cols-1 : 
-                    return length  
-
-                directions = [[0 , 1] ,[0 ,-1] , [1 , 0] , [-1 , 0] , [1 , 1] , [-1 , 1] , [1 , -1] , [-1 , -1]]
-
-                for dr , dc in directions : 
-                    r , c = row + dr , col + dc
-
-                    if r in range(rows) and c in range(cols) and (r , c) not in visited and grid[r][c] == 0 : 
-                        q.append((r , c , length+1))
-                        visited.add((r, c))
-            return -1
-        
         if grid[0][0] != 0 or grid[rows-1][cols-1] !=0 : 
             return -1
+        
+        if rows == 1 and cols == 1: return 1 
 
-        return bfs(0 , 0)
+        visited = set()
+        q = deque()
+        visited.add((0 , 0))
+        q.append((0 , 0 , 1))
+        
+
+        while q : 
+            row , col, length = q.popleft()
+
+            if row == rows-1 and col == cols-1 : 
+                return length  
+
+            directions = [[0 , 1] ,[0 ,-1] , [1 , 0] , [-1 , 0] , [1 , 1] , [-1 , 1] , [1 , -1] , [-1 , -1]]
+
+            for dr , dc in directions : 
+                r , c = row + dr , col + dc
+
+                if r in range(rows) and c in range(cols) and (r , c) not in visited and grid[r][c] == 0 : 
+                    q.append((r , c , length+1))
+                    visited.add((r, c))
+        return -1
+    
+    
+
 
                     
