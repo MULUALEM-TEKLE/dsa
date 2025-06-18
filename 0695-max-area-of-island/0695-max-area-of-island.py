e20a53eb512c7227c3973d52792973ffc1499e6c
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-18 07:37:26 +0300

    Time: 56 ms (5.08%), Space: 19.5 MB (22.98%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,37 +1,20 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid : 
-            return 0
-
+        rows, cols = len(grid) , len(grid[0])
         visited = set()
-        rows , cols = len(grid) , len(grid[0])
-        island_area = 0
 
-        def bfs(r , c) : 
-            q = deque()
-            q.append((r , c))
+        def dfs(r , c) : 
+            if r not in range(rows) or c not in range(cols) or grid[r][c] == 0 or (r , c) in visited : 
+                return 0
+            
             visited.add((r , c))
-            area = 1
-
-            while q : 
-                row , col = q.popleft()
-                directions = [[1 , 0] , [-1 , 0] , [0 , 1] , [0 , -1]]
-
-                for dr,dc in directions : 
-                    r , c = row + dr , col + dc
-
-                    if r in range(rows) and c in range(cols) and (r , c) not in visited and grid[r][c] == 1 : 
-                        q.append((r , c))
-                        visited.add((r , c))
-                        area += 1
-            return area
-
+            return 1 + dfs(r-1 ,c) + dfs(r+1 ,c) + dfs(r ,c-1) + dfs(r ,c+1)
+        
+        area = 0 
 
         for r in range(rows) : 
             for c in range(cols) : 
-                if grid[r][c] == 1 and (r , c) not in visited : 
-                    print("calling bfs")
-                    island_area = max(island_area ,bfs(r , c))
+                if grid[r][c] == 1 : 
+                    area = max(area, dfs(r,c))
         
-        return island_area
-        # return 0
+        return area
