ffc3261dd22c1019ab3f10aa4cbdb19fce2cad58
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-19 08:52:25 +0300

    Time: 4 ms (55.41%), Space: 17.8 MB (56.74%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,25 +6,25 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         total = carry = 0
-        result = ListNode()
-        current = result 
+
+        dummy = ListNode(0)
+        cur = dummy
 
         while l1 or l2 or carry : 
             total = carry 
-            
+
             if l1 : 
                 total += l1.val 
                 l1 = l1.next 
             
             if l2 : 
-                total += l2.val
+                total += l2.val 
                 l2 = l2.next 
-
+            
+            res = total % 10 
             carry = total // 10
-            num = total % 10 
+            cur.next = ListNode(res)
+            cur = cur.next 
 
-            current.next = ListNode(num)
-            current = current.next 
+        return dummy.next
 
-        return result.next 
-        
