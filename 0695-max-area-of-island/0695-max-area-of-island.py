68c1c9ada4fe5af1f25e9edd731372121d201994
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-21 17:25:35 +0300

    Time: 27 ms (42.87%), Space: 18.3 MB (52.85%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,17 +1,28 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows , cols = len(grid) , len(grid[0])
+        dxn = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
         visited = set()
 
-        def dfs(r , c) : 
-            if r not in range(rows) or c not in range(cols) or (r , c) in visited or grid[r][c] == 0 : 
-                return 0
-            visited.add((r , c))
-            return 1 + dfs(r+1 ,c) + dfs(r-1 , c) + dfs(r , c+1) + dfs(r , c-1)
+        def bfs(r , c) : 
+            q = deque()
+            q.append((r,c))
+            visited.add((r,c))
+            island_area = 1
+            while q: 
+                row , col = q.popleft()
+                for dr,dc in dxn : 
+                    r , c = row + dr , col + dc
+                    if r in range(rows) and c in range(cols) and (r , c) not in visited and grid[r][c] == 1 : 
+                        q.append((r, c))
+                        visited.add((r , c))
+                        island_area += 1 
+            return island_area
+            
 
         area = 0
         for r in range(rows) : 
             for c in range(cols) : 
                 if grid[r][c] == 1 and (r , c) not in visited: 
-                    area = max(area , dfs(r , c))
+                    area = max(area , bfs(r , c))
         return area
