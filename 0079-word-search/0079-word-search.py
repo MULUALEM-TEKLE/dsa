1065a6a9d6cc0fe1988463aad9490ee338e5585e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-21 17:42:06 +0300

    Time: 1107 ms (93.65%), Space: 17.9 MB (39.88%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,48 +1,38 @@
-# from collections import Counter
-
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board) , len(board[0])
-        path = set()
+        rows, cols = len(board), len(board[0])
+        visited = set()
 
-        board_char_counts = {}
-        for r in range(rows):
-            for c in range(cols):
-                board_char_counts[board[r][c]] = board_char_counts.get(board[r][c] , 0) + 1 
-
-        word_char_counts = {}
-
-        for char in word : 
-            word_char_counts[char] = word_char_counts.get(char , 0) + 1
-            
-        for char, count in word_char_counts.items():
-            if char not in board_char_counts or board_char_counts[char] < count:
+        board_letters_counter = Counter() 
+        for r in range(rows) : 
+            for c in range(cols) : 
+                board_letters_counter[board[r][c]] += 1 
+        word_letter_counter = Counter(word)
+        
+        for char, count in word_letter_counter.items(): 
+            if board_letters_counter[char] < count : 
                 return False
-
-        # Also, for an edge case: if the word is longer than the total number of cells, it can't exist.
-        if len(word) > rows * cols:
-            return False
+        
+        if len(word) > rows * cols : 
+            return False 
 
         def dfs(r , c , i) : 
-            if i == len(word)  : 
-                return True 
+            if i == len(word) : 
+                return True
             
-            if r not in range(rows) or c not in range(cols) or word[i] != board[r][c] or (r , c) in path : 
-                return False
-
+            if r not in range(rows) or c not in range(cols) or board[r][c] != word[i] or (r , c) in visited : 
+                return False 
             
-            path.add((r , c))
-            res = dfs(r+1 , c , i+1) or dfs(r-1 , c , i+1) or dfs(r , c+1 , i+1) or dfs(r , c-1 , i+1)
-            path.remove((r ,c))
+            visited.add((r,c))
+            res = dfs(r+1 , c , i+1) or dfs(r-1 , c , i+1) or dfs(r , c+1 , i+1) or dfs(r , c-1 , i+1) 
+            visited.remove((r , c))
             return res
-        
-        res = False
 
-        for r in range(rows): 
+        is_word_there = False
+        for r in range(rows) : 
             for c in range(cols) : 
-                if board[r][c] == word[0] : 
-                    res = res or dfs(r , c , 0)
-                    if res : 
+                if board[r][c] == word[0] and (r, c) not in visited : 
+                    is_word_there = is_word_there or dfs(r , c , 0)
+                    if is_word_there : 
                         return True
-        
         return False
