b16b6e53e4ec1a96c1b69e303070c0d8a2b324a8
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-21 18:11:28 +0300

    Time: 5 ms (35.12%), Space: 17.8 MB (44.44%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,32 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows , cols = len(grid) , len(grid[0])
+        dxn = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
 
-        q = deque()
-        fresh_oranges = 0
+        rotten = deque()
+        fresh = 0
 
         for r in range(rows) : 
             for c in range(cols) : 
                 if grid[r][c] == 2 : 
-                    q.append((r , c))
-                if grid[r][c] == 1 : 
-                    fresh_oranges +=1 
+                    rotten.append((r, c))
+                elif grid[r][c] == 1 : 
+                    fresh += 1 
         minutes = 0
-        while q and fresh_oranges > 0: 
-            for _ in range(len(q)) : 
-                row , col = q.popleft()
-                dxn = [[1 ,0] , [-1 , 0] , [0 , 1] , [0 , -1]]
-                for dr, dc in dxn : 
+        while rotten and fresh > 0 : 
+            for _ in range(len(rotten)) : 
+                row , col = rotten.popleft()
+                for dr , dc in dxn : 
                     r , c = row + dr , col + dc
                     if r in range(rows) and c in range(cols) and grid[r][c] == 1 : 
-                        grid[r][c] = 2
-                        q.append((r , c))
-                        fresh_oranges -= 1 
-            
-            if q : 
-                minutes += 1
+                        grid[r][c] = 2 
+                        fresh -= 1
+                        rotten.append((r , c))
+                        
+            minutes += 1   
         
-        if fresh_oranges == 0 : 
+        if fresh == 0 : 
             return minutes 
         else : 
             return -1
