2b2da6341c056e6b8159d074a7cc29503635c9ea
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-25 16:07:47 +0300

    Time: 50 ms (56.48%), Space: 34.6 MB (45.36%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,8 +1,13 @@
+def isPali(s , l , r) : 
+    while l < r : 
+        if s[l] != s[r] : 
+            return False
+        l += 1 
+        r -= 1
+    return True
+
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        if len(s) == 1 : 
-            return [[s]]
-        
         res = []
 
         part = []
@@ -10,18 +15,14 @@ class Solution:
             if i >= len(s) : 
                 res.append(part[:])
                 return 
-
-            for j in range(i , len(s)) : 
-                if self.isPali(s , i , j) : 
+            
+            for j in range(i, len(s)) : 
+                if isPali(s , i , j) : 
                     part.append(s[i : j+1])
                     dfs(j+1)
                     part.pop()
         dfs(0)
-        return res 
+        return res
 
-    def isPali(self, s , l , r) : 
-        while l < r : 
-            if s[l] != s[r] : 
-                return False 
-            l , r = l+1 , r-1
-        return True
+
+        
