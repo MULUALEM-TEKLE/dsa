0a8880d3f5e98764eb5f6af636924b36cf0b95dc
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-26 12:07:15 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,28 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pivot = None 
-
+        pivot = None
         for i in range(len(nums)-1 , 0 , -1) : 
             if nums[i] > nums[i-1] : 
                 pivot = i-1
                 break
         else : 
-            nums.reverse()
-            return nums 
+            return nums[::-1]
         
-        swap = len(nums) - 1
+        swap = len(nums)-1
 
         while nums[swap] <= nums[pivot] : 
             swap -= 1
 
-        nums[pivot], nums[swap] = nums[swap] , nums[pivot]
-
-        nums[pivot+1 : ] = reversed(nums[pivot+1 :])
-
-        return nums 
-
-"""
-Time complexity : o(N)
-Space complexity : o(1)
-"""
+        nums[swap] , nums[pivot] = nums[pivot] , nums[swap]
+        nums[pivot+1:] = reversed(nums[pivot+1 :])
+        return nums
