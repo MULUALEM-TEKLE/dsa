e5542ecc990aa2bb95d23ffe02d9fd84c747f833
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-30 11:27:57 +0300

    Time: 612 ms (37.64%), Space: 20.3 MB (93.22%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,8 +4,8 @@ class Solution:
         nums.sort()
         
         for index , num in enumerate(nums) : 
-            # if index > 0 and num == nums[index - 1]: 
-            #     continue
+            if index > 0 and num == nums[index - 1]: 
+                continue
             
             left, right = index + 1 , len(nums) -1 
 
@@ -15,7 +15,8 @@ class Solution:
                 elif num + nums[left] + nums[right] < 0 :
                     left += 1  
                 else : 
-                    output.add((nums[left], nums[right], num))
+                    res = (nums[left], nums[right], num)
+                    output.add(res)
                     left += 1 
                     right -= 1 
                     # while nums[left] == nums[left - 1 ] and left < right : 
