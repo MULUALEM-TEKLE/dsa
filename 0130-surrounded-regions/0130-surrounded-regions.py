86be3e70661ee4625d77bc3a8b3026eb627ed316
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-06-30 22:43:24 +0300

    Time: 75 ms (6.66%), Space: 30.4 MB (9.08%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,50 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        left , right , top , bottom = 0 , len(board[0]) , 0 , len(board)
+        edges = set()
+        for i in range(left , right) : 
+            edges.add((top , i))
+            edges.add((bottom-1 , i))
+        for i in range(top+1 , bottom-1) : 
+            edges.add((i, left))
+            edges.add((i , right-1 ))
+
+        visited = set()
+        def bfs(r , c) :
+            q = deque()
+            visited.add((r , c))
+            q.append((r , c))
+            mark = set()
+            mark.add((r , c))
+            safe = True
+
+            while q : 
+                row , col = q.popleft()
+                if (row , col) in edges : 
+                    safe = False
+                directions = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]] 
+                for dr , dc in directions : 
+                    r , c = row + dr , col + dc 
+                    if r in range(rows) and c in range(cols) and (r,c) not in visited and board[r][c] == "O" : 
+                        mark.add((r,c))
+                        q.append((r , c))
+                        visited.add((r , c))
+            if safe : 
+                for row,col in mark : 
+                    board[row][col] = "X"
+
+        rows , cols = len(board) , len(board[0])
+
+        print(edges)
+
+        for r in range(rows) : 
+            for c in range(cols) : 
+                if board[r][c] == "O" and (r , c) not in visited  : 
+                    bfs(r , c)
+        
+        
+
+        
