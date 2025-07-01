d00f73152500ece8d14771857105b8ce0affa654
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-01 20:38:57 +0300

    Time: 0 ms (100%), Space: 17.9 MB (22.93%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,19 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        res = []
         table = {
-            "2" : "abc" , "3" : "def", "4" : "ghi" , "5" : "jkl" , "6" : "mno" , "7" : "pqrs" , "8" : "tuv" , "9" : "wxyz"
+            "2" : "abc" , 
+            "3" : "def" , 
+            "4" : "ghi" , 
+            "5" : "jkl" , 
+            "6" : "mno" , 
+            "7" : "pqrs" , 
+            "8" : "tuv" , 
+            "9" : "wxyz" , 
         }
 
-        def backtrack(i , cur) : 
-            if len(cur) == len(digits) : 
-                res.append(cur)
+        res = []
+        def dfs(i , cur_str) :
+            if i >= len(digits) : 
+                res.append(cur_str)
                 return 
-
             for c in table[digits[i]] : 
-                backtrack(i+1 , cur + c)
-                
-        if digits : 
-            backtrack(0 , "")
+                dfs(i+1 , cur_str + c)
+        if not digits == "" :
+            dfs(0 , "")
         return res 
 
+
+       
