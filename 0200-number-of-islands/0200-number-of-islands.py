d3f85f4a4fafc18311373e4951205f7648cc8e96
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-01 09:39:48 +0300

    Time: 248 ms (56.02%), Space: 29.1 MB (5.99%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid : 
-            return 0
-        
-        rows , cols  = len(grid) , len(grid[0])
+        rows , cols = len(grid) , len(grid[0])
         visited = set()
-        islands = 0
 
-        def bfs(r , c) : 
-            q = deque()
+        def dfs(r, c) : 
+            # base case 
+            if r < 0 or c < 0 or r == rows or c == cols or grid[r][c] == "0" or (r,c) in visited : 
+                return 0
+
+            # recursive case 
             visited.add((r , c))
-            q.append((r , c))
+            
+            dfs(r+1 , c) 
+            dfs(r-1 , c) 
+            dfs(r , c+1) 
+            dfs(r , c-1)
 
-            while q : 
-                row , col = q.popleft()
-                directions  = [[1 , 0] , [-1 , 0] , [0 , 1] , [0 , -1]]
+            return 1
 
-                for dr,dc in directions : 
-                    r , c = row + dr , col + dc
-                    if (r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r , c) not in visited) :
-                        q.append((r , c))
-                        visited.add((r , c))
-
-
+        count = 0
         for r in range(rows) : 
             for c in range(cols) : 
-                if grid[r][c] == "1" and (r , c) not in visited : 
-                    bfs(r , c)
-                    islands += 1 
-        
-        return islands
+                if grid[r][c] == "1" and (r,c) not in visited : 
+                    count += dfs(r , c)
+        return count
