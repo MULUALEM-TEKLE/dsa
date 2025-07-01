608206bd0856cc865ba66ac1c018d1011bce59b8
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-01 09:50:09 +0300

    Time: 2 ms (93.72%), Space: 17.7 MB (96.96%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -2,31 +2,21 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         res = []
-
-        subset = []
-        def dfs(i , target_num) : 
+        comb = []
+        def dfs(i , cur) : 
+            if cur > target : return
+            if cur == target : 
+                res.append(comb[:])
+                return
             if i >= len(candidates) : 
+                return 
+            if candidates[i] + cur > target : 
                 return
-            
-            target_num -= candidates[i]
-            if target_num < 0 : 
-                target_num += candidates[i]
-                return
-                
-            subset.append(candidates[i])
-
-
-
-            # print(target_num)
-            if target_num == 0 : 
-                res.append(subset[:])
-
-            dfs(i + 1 , target_num)
-            subset.pop()
-            target_num += candidates[i]
-            while i+1 < len(candidates) and candidates[i] == candidates[i+1] : 
-                i += 1 
-            dfs(i+1 , target_num)
-        
-        dfs(0 , target)
+            comb.append(candidates[i])
+            dfs(i+1 , cur + candidates[i])
+            comb.pop()
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]: 
+                i+=1
+            dfs(i+1 , cur)
+        dfs(0,0)
         return res
