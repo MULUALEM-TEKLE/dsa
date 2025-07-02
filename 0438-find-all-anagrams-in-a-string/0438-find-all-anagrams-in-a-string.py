4c235f9ec8bfd0a57da03a0cf44da8e14d27d4db
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-02 11:31:06 +0300

    Time: 5761 ms (12.36%), Space: 18.1 MB (94.35%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,19 +1,14 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        res = []
-        left = 0
-        def sort_word(word) : 
-            word = list(word)
-            word.sort()
-            word = "".join(word)
-            return word 
-        p = sort_word(p)
+        p_table = Counter(p)
         N = len(p)
+        left = 0
+        window_table = Counter(s[0 : N])
+        res = []
         while left < len(s)-N+1 : 
-            if s[left] in p and sort_word(s[left : left+N]) == p : 
+            window_table = Counter(s[left : left+N])
+            if window_table == p_table : 
                 res.append(left)
-                left += 1
-            else : 
-                left += 1 
-            
+            # window_table = {}
+            left += 1 
         return res 
