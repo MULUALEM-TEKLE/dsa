7dfa1f23d815c93a5a672b30bc2de8ef5d2bb6a8
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-05 10:21:58 +0300

    Time: 143 ms (53.09%), Space: 77.9 MB (96.62%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,46 +1,62 @@
 class Node : 
-    def __init__(self, key, val):
-        self.key, self.val = key, val 
-        self.prev = self.next = None
-
+    def __init__(self, key=None , val=None , prev=None, next=None) : 
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
 
-        self.left , self.right = Node(0 ,0) , Node(0 , 0)
+        self.left = Node()
+        self.right = Node()
+
         self.left.next , self.right.prev = self.right , self.left
     
     def remove(self, node) : 
-        prev , nxt = node.prev , node.next 
-        prev.next , nxt.prev = nxt , prev
+        # connect the node prev to this node and the next node of current node
+        node.prev.next = node.next 
+        node.next.prev = node.prev
     
-    def insert(self, node):
-        prev , nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node 
-        node.next = nxt 
-        node.prev = prev
+    def insert(self, node) : 
+        # here we gotta insert at the right end, since whatever we insert should always be 
+        # MRU so at the right end we insert the current node with the correct pointer update
+        MRU = self.right.prev 
+        MRU.next = node
+        node.prev = MRU
+        node.next = self.right
+        self.right.prev = node
+        
 
     def get(self, key: int) -> int:
+        # we gotta check on the cache, if so return the val, and remove and insert the node so it becomes the MRU
         if key in self.cache : 
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
         return -1
+
         
 
     def put(self, key: int, value: int) -> None:
+        # first check if the node is in our cache, if so we just update its value, if not we check if our capacity is full if so remove MRU and add the node at the MRU end
         if key in self.cache : 
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-
-        if len(self.cache) > self.cap : 
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-
+            node = self.cache[key]
+            node.val = value 
+            self.remove(node)
+            self.insert(node)
+        else : 
+            new_node = Node(key, value)
+            self.insert(new_node)
+            self.cache[key] = new_node
+            if len(self.cache) > self.cap : 
+                lru = self.left.next
+                self.remove(lru)
+                del self.cache[lru.key]
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
