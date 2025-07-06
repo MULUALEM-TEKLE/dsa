125262041ee23323358b4389afad9d9f8ac07887
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-06 21:43:28 +0300

    Time: 0 ms (100%), Space: 17.7 MB (86.16%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,10 +4,10 @@ class Solution:
         if n == 0 : return 
         if n == 1 : return nums[0]
         if n == 2 : return max(nums[0] , nums[1])
-        dp = [0 , 0]
-        for i in range(n-1) : 
+        dp = [nums[0] , max(nums[0] , nums[1])]
+        for i in range(2 , n-1) : 
             dp[0] , dp[1] = dp[1] , max(dp[1] , nums[i] + dp[0])
-        dpp = [0 , 0]
-        for i in range(1 , n) : 
+        dpp = [nums[1] , max(nums[1] , nums[2])]
+        for i in range(3 , n) : 
             dpp[0] , dpp[1] = dpp[1] , max(dpp[1] , nums[i] + dpp[0])
         return max(dp[1] , dpp[1])
