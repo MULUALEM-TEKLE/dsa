3bf929e96e5cbd00ddd8f354327c8cabcb542ff1
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-06 21:59:23 +0300

    Time: 0 ms (100%), Space: 18 MB (17.23%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,13 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 0 : return 
-        if n == 1 : return nums[0]
-        if n == 2 : return max(nums[0] , nums[1])
-        dp = [nums[0] , max(nums[0] , nums[1])]
-        for i in range(2 , n-1) : 
-            dp[0] , dp[1] = dp[1] , max(dp[1] , nums[i] + dp[0])
-        dpp = [nums[1] , max(nums[1] , nums[2])]
-        for i in range(3 , n) : 
-            dpp[0] , dpp[1] = dpp[1] , max(dpp[1] , nums[i] + dpp[0])
-        return max(dp[1] , dpp[1])
+        if n == 1 : 
+            return nums[0]
+        if n == 2 : 
+            return max(nums[0] , nums[1])
+
+        def steal(sub) : 
+            dp = [sub[0] , max(sub[0] , sub[1])]
+            for n in range(2 , len(sub)) : 
+                dp[0] , dp[1] = dp[1] , max(dp[0]+sub[n] , dp[1])
+            return dp[1]
+        
+        return max(steal(nums[:-1]) , steal(nums[1:]))
