46e234d1409cedc7617732e88eff592bc427d953
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-06 21:10:30 +0300

    Time: 0 ms (100%), Space: 18 MB (13.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        one = two = 0
-
-        for n in nums : 
-            tmp = max(n + one , two)
-            one = two 
-            two = tmp 
-        return two
+        n = len(nums)
+        if n == 1 : 
+                return nums[0]
+        if n == 2 : 
+            return max(nums[0] , nums[1])
+        memo = {0 : nums[0] , 1 :max(nums[0] , nums[1]) }
+        def steal(n) : 
+            if n in memo : 
+                return memo[n]
+           
+            memo[n] = max(nums[n]+steal(n-2) , steal(n-1))
+            return memo[n]
+        return steal(n-1)
