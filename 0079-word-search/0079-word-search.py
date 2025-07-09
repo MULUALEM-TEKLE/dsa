01de60765c3c3663ee0a02477b4e0ef399f9a435
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-09 09:08:23 +0300

    Time: 3 ms (99.01%), Space: 17.8 MB (87.37%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,11 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows , cols = len(board) , len(board[0])
+
+        board_counter = defaultdict(int)
+        word_counter = Counter(word)
+
+        if len(word) > rows*cols : 
+            return False
+
+        if word_counter[word[0]] > word_counter[word[-1]] : 
+            word = word[::-1]
+
         start_pos = []
         for r in range(rows) : 
             for c in range(cols) : 
                 if board[r][c] == word[0] : 
                     start_pos.append((r ,c))
+                board_counter[board[r][c]] += 1 
+        
+        for letter,count in word_counter.items() : 
+            if count > board_counter[letter] : 
+                return False
+        
         visited = set()
         def dfs(r , c , idx) : 
             if len(word) == idx : 
