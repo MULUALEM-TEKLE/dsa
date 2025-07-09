1261c62210c147b182fd3d927cf6a8d39f2c2e7b
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-09 09:04:04 +0300

    Time: 9353 ms (5%), Space: 17.9 MB (39.79%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,36 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def in_bound(x, y):
-            return 0 <= x < len(board) and 0 <= y < len(board[0]) 
-    
-        def direction(x, y):
-            neighbors = []
-            for i, j in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                if in_bound(x + i, y + j):
-                    neighbors.append((x + i, y + j))
-            return neighbors
+        rows , cols = len(board) , len(board[0])
+        start_pos = []
+        for r in range(rows) : 
+            for c in range(cols) : 
+                if board[r][c] == word[0] : 
+                    start_pos.append((r ,c))
+        visited = set()
+        def dfs(r , c , idx) : 
+            if len(word) == idx : 
+                return True
+            if r not in range(rows) or c not in range(cols) or board[r][c] != word[idx] or (r , c) in visited: 
+                return False
+            visited.add((r , c))
+            dxn = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
+            res = False 
+            for dr,dc in dxn : 
+                res = res or dfs(r+dr , c+dc , idx+1)
+            visited.remove((r, c))
+            return res
+
         
-        # Check for each starting positions
-        def dfs_check(curr, i, j, visited):
-            visited.add((i, j))
-            if curr + 1 == len(word):
-                return True
-            for x, y in direction(i, j): 
-                if board[x][y] == word[curr + 1] and (x, y) not in visited:
-                    visited.add((x, y))
-                    if dfs_check(curr + 1, x, y, visited):
-                        return True
-            visited.remove((i, j))
-            return False
-
-        # collect word freq.
-        locations = defaultdict(list)
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                locations[board[i][j]].append([i, j])
-
-        # run dfs check
-        for i, j in locations[word[0]]:
-            if dfs_check(0, i, j, set()):
-                return True
+        for r,c in start_pos : 
+            if dfs(r , c , 0) : 
+                return True 
         return False
