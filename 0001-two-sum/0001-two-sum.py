30036a938ee5ceddf0c22817311a7f253708fa1d
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-09 09:18:33 +0300

    Time: 283 ms (34.67%), Space: 18.6 MB (71.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        for index, num in enumerate(nums) : 
-            if num in map : 
-                map[num].append(index)
-            else : 
-                map[num] = [index]
-
-        for num in nums : 
-            if target - num in map  :
-                print(map[num])
-                if num == target-num and len(map[num]) == 2 : 
-                    return map[num] 
-                if map[target-num] != map[num] :
-                    return [map[num][0] , map[target-num][0]]
+        table = defaultdict(int)
+        for idx,num in enumerate(nums) :
+            rest = nums[idx+1:] 
+            if target-num in rest : 
+                return [idx , rest.index(target-num)+idx+1]
+             
