5c463ca8db15e364750d6abb8f414987410dd1ef
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-13 12:19:42 +0300

    Time: 1105 ms (12.49%), Space: 413.5 MB (5.01%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,13 +1,8 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        memo = {}
-        def ways(i,amt) : 
+        @cache
+        def ways(i , amt) : 
             if amt == 0 : return 1 
             if i == len(coins) or amt < 0 : return 0
-            if (i,amt) in memo : return memo[(i,amt)]
-            memo[(i,amt)] = ways(i , amt-coins[i]) + ways(i+1 , amt)
-            return memo[(i,amt)]
-        res = ways(0 , amount)
-        # print(memo)
-        return res
-            
+            return ways(i , amt-coins[i]) + ways(i+1 , amt)
+        return ways(0 , amount)
