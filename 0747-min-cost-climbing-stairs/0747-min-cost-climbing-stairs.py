1e34d88b5fc085297fe9e80533f929dae4d4a7c0
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-13 22:01:58 +0300

    Time: 7 ms (20.97%), Space: 19.9 MB (13.73%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        @cache
+        memo = {0:0 , 1:0}
         def climb(i) : 
-            if i < 2 :
-                return 0 
-            return min(climb(i-1) + cost[i-1] , climb(i-2) + cost[i-2])
+            if i in memo : return memo[i]
+            memo[i] = min(climb(i-1) + cost[i-1] , climb(i-2) + cost[i-2])
+            return memo[i]
         return climb(len(cost))
