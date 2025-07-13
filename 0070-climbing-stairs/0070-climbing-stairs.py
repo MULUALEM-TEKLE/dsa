11c6ce6d9d4939f64da4ec3ebfb19229f3dc0304
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-13 18:28:55 +0300

    Time: 3 ms (2.86%), Space: 17.7 MB (78.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,44 @@
+'''
+You are climbing a staircase. 
+It takes n steps to reach the top.
+    -> height of the staircase is n
+
+Each time you can either climb 1 or 2 steps.
+    -> either increment or decrement 1 or 2 steps 
+    -> decrement or increment from n
+
+In how many distinct ways can you climb to the top?
+    -> counting 
+    -> combinatorics
+    -> distinct == unique
+
+Sample 
+n = 2
+1. 1 step + 1 step
+2. 2 steps
+res = 2
+
+Base cases
+    -> from step 0 there is 1 way to reach the 0th step
+    -> from step 1 there is 1 way to reach the 0th step
+    -> from step 2  there area 2 ways to reach the 0th step
+            -> 2 -> 1 -> 0 
+            -> 2 -> 0
+how many ways to reach step 0 from step n
+    -> ways it take to reach step n-1 and step n-2
+    -> why?
+-> n can not be less than 0
+'''
+
+
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 0 : 
-            return 0
-        if n == 1 : 
-            return 1 
-        dp = [1 , 1]
-        for i in range(1 , n) : 
-            dp[0] , dp[1] = dp[1] , dp[0]+dp[1]
-        return dp[1]
-
+        @cache
+        def climb(n) : 
+            if n == 1 : return 1
+            if n == 2 : return 2
+            return climb(n-2) + climb(n-1)
         
+        return climb(n)
+         
