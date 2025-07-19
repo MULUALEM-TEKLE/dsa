1f60a0bce27e9acaaf7008369cf320cd23f9adb2
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-19 22:25:35 +0300

    Time: 12 ms (72.94%), Space: 20.3 MB (40.34%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,21 +1,12 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         rows , cols = len(grid) , len(grid[0])
-        visited = set()
-        memo = {}
-        def dfs(r , c) : 
-            if (r , c) in memo : return memo[(r , c)]
-            if r not in range(rows) or c not in range(cols) or (r , c) in visited : 
-                return float('inf')
-            
-            if r == rows-1 and c == cols-1 : 
-                return grid[r][c]
+        dp = [[float('inf')] * (cols+1) for _ in range(rows+1)]
+        dp[1][1] = grid[0][0]
 
-            visited.add((r , c))
-            res = min(dfs(r+1 , c ) , dfs(r , c+1)) + grid[r][c]
-            visited.remove((r , c))
-
-            memo[(r , c)] = res
-            return memo[(r , c)]
-
-        return  dfs(0 , 0) 
+        for row in range(1 , rows+1) : 
+            for col in range(1 , cols+1) : 
+                if row == col == 1 : continue
+                dp[row][col] = min(dp[row-1][col] , dp[row][col-1]) + grid[row-1][col-1]
+        
+        return dp[rows][cols]
