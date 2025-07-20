32e93041d8e09003605d2594372ac1bccbb12332
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-20 20:35:59 +0300

    Time: 4 ms (3.57%), Space: 18 MB (12.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,34 +1,9 @@
-# class Solution:
-#     def rob(self, nums: List[int]) -> int:
-#         def steal(n) :
-#             if n == 0 : 
-#                 return nums[0]
-#             if n == 1 : 
-#                 return max(nums[0] , nums[1])
-#             return max(nums[n]+steal(n-2) , steal(n-1))
-#         return steal(len(nums)-1)
-
-# class Solution:
-#     def rob(self, nums: List[int]) -> int:
-#         if len(nums) == 1 : return nums[0]
-#         memo = {0 : nums[0] , 1 : max(nums[0] , nums[1])}
-#         def steal(n) :
-#             if n in memo : 
-#                 return memo[n]
-#             memo[n] = max(nums[n]+steal(n-2) , steal(n-1))
-#             return memo[n]
-#         return steal(len(nums)-1)
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1 : 
-            return nums[0]
-        if n == 2 : 
-            return max(nums[0] , nums[1])
-        
-        dp = [nums[0] , max(nums[0] , nums[1])]
-        for i in range(2 , n) :
-            dp[0] , dp[1] = dp[1] , max(dp[0] + nums[i] , dp[1] ) 
-        return dp[1]
-            
+        @cache
+        def steal(n) : 
+            if n < 0 : 
+                return 0
+            return max(nums[n] + steal(n-2) , steal(n-1))
+
+        return steal(len(nums)-1)
