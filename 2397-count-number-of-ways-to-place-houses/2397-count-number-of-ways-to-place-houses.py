4313f67e610f3fb908c8013fb19927d5b6fcbcfc
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-21 08:25:04 +0300

    Time: 76 ms (56.57%), Space: 17.9 MB (76.49%) - LeetHub

--- 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.py
+++ 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.py
@@ -1,5 +1,7 @@
 class Solution:
     def countHousePlacements(self, n: int) -> int:
+        if n == 0 : return 1 
+        if n == 1 : return 4 
         dp = [1 , 2]
         for i in range(2 , n+1) : 
             dp[0] , dp[1] = dp[1],  dp[0] + dp[1]
