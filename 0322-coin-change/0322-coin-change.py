bf4b9b4f094e857d37798148f099671658203ae8
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-21 18:08:30 +0300

    Time: 917 ms (32.49%), Space: 32.8 MB (19.15%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -6,10 +6,9 @@ class Solution:
             if amt <= 0 : return 0
             minm = float('inf')
             for coin in coins : 
-                diff = amt-coin
+                diff = amt - coin 
                 if diff < 0 : break 
-                minm = min(minm , 1+change(diff))
-            return minm 
-        
+                minm = min(minm , change(diff)+1)
+            return minm
         res = change(amount)
         return res if res != float('inf') else -1
