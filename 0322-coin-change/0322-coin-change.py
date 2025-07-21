e8f3160d16257493b32064c39c24f069ca01ba5a
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-21 18:14:54 +0300

    Time: 1223 ms (13.74%), Space: 30.4 MB (20.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         coins.sort()
-        @cache
-        def change(amt) : 
-            if amt <= 0 : return 0
-            minm = float('inf')
+        memo ={0:0}
+
+        def find_ways(amt) : 
+            if amt in memo : return memo[amt]
+            minm = float("inf")
             for coin in coins : 
                 diff = amt - coin 
-                if diff < 0 : break 
-                minm = min(minm , change(diff)+1)
-            return minm
-        res = change(amount)
-        return res if res != float('inf') else -1
+                if diff < 0 : 
+                    break
+                minm = min(minm , 1+find_ways(diff))
+            memo[amt] = minm
+            return memo[amt]
+                
+        
+        result = find_ways(amount)
+        print(memo)
+        if result < float("inf") : return result
+        return -1
