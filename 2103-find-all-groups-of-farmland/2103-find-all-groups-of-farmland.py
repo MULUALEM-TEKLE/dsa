07cc35fb999b0253ae3a1e0cbd0e52ce12bc895b
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-23 17:45:34 +0300

    Time: 601 ms (7.9%), Space: 40.4 MB (24.06%) - LeetHub

--- 2103-find-all-groups-of-farmland/2103-find-all-groups-of-farmland.py
+++ 2103-find-all-groups-of-farmland/2103-find-all-groups-of-farmland.py
@@ -1,32 +1,30 @@
 class Solution:
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
         rows , cols = len(land) , len(land[0])
-        directions = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
-        res = []
         visited = set()
+        res = []
 
         def bfs(r , c) : 
             q = deque()
             q.append((r , c))
             visited.add((r , c))
-            location = [r , c]
 
-            end = []
+            location = [r , c]
+            end = None
             while q : 
                 row , col = q.popleft()
                 end = [row , col]
-                for dr , dc in directions : 
+                directions = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
+                for dr,dc in directions : 
                     r , c = row+dr , col+dc
                     if r in range(rows) and c in range(cols) and (r , c) not in visited and land[r][c] == 1 : 
                         q.append((r , c))
                         visited.add((r , c))
-            
             location.extend(end)
-            print(location)
             res.append(location)
-        for r in range(rows) : 
-            for c in range(cols) : 
+        
+        for r in range(rows): 
+            for c in range(cols): 
                 if land[r][c] == 1 and (r , c) not in visited : 
                     bfs(r , c)
-        
         return res
