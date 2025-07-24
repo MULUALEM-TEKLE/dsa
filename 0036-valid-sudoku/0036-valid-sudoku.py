5263a23ff50c44e8f084a6a26f101ed38fb9f65e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-24 10:58:27 +0300

    Time: 3 ms (82.12%), Space: 18 MB (24.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,37 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for r in range(9) : 
+        N = 9 
+        for r in range(N) : 
             s = set()
-            for c in range(9) : 
-                item = board[r][c]
-                if item in s : 
-                    return False
-                elif item != "." : 
-                    s.add(item) 
-        for r in range(9) : 
+            for c in range(N) : 
+                if board[r][c] == "." : continue 
+                if board[r][c] in s : return False 
+                s.add(board[r][c])
+            
+        for c in range(N) : 
             s = set()
-            for c in range(9) : 
-                item = board[c][r]
-                if item in s : 
-                    return False
-                elif item != "." : 
-                    s.add(item) 
+            for r in range(N) : 
+                if board[r][c] == "." : continue 
+                if board[r][c] in s : return False 
+                s.add(board[r][c])
         
-        starts = [
-            (0 ,0) , (0 , 3) , (0 , 6) , 
-            (3 ,0) , (3 , 3) , (3 , 6) , 
-            (6 ,0) , (6 , 3) , (6 , 6) , 
-        ]
-
-        for i,j in starts : 
+        start = [
+            [0 , 0] , [0 , 3] , [0 , 6] , 
+            [3 , 0] , [3 , 3] , [3 , 6] , 
+            [6 , 0] , [6 , 3] , [6 , 6] , 
+            ]
+        
+        for i,j in start : 
             s = set()
-            for row in range(i , i+3) : 
-                for col in range(j , j+3) : 
-                    item = board[row][col] 
-                    if item in s : 
-                        return False
-                    elif item != "." : 
-                        s.add(item)
-
+            for r in range(i , i+3) : 
+                for c in range(j , j+3) : 
+                    if board[r][c] == "." : continue 
+                    if board[r][c] in s : return False 
+                    s.add(board[r][c])
+        
         return True
-
