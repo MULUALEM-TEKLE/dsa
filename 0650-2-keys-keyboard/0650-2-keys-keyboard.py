4e2edd9ff4d6ea8df882bb5025c764a9d53a3abc
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-25 12:43:57 +0300

    Time: 881 ms (6.47%), Space: 17.7 MB (79.37%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        if n == 1 : return 0
+        def do(count , paste) : 
+            if count == n : return 0
+            if count > n : return float('inf')
+            paste = 1 + do(count+paste , paste)
+            copy = 2 + do(count+count , count)
+
+            return min(copy , paste)
+        
+        return 1 + do(1 , 1)
