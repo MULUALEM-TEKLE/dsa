31e2b8c92a9d449f4c629a7f7dee2f1e7a710f45
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-28 13:53:02 +0300

    Time: 108 ms (98.13%), Space: 17.8 MB (92.64%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,8 +1,11 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def ways(i , amt) : 
-            if amt == 0 : return 1 
-            if i == len(coins) or amt < 0 : return 0
-            return ways(i , amt-coins[i]) + ways(i+1 , amt)
-        return ways(0 , amount)
+        dp = [0] * (amount+1)
+        dp[0] = 1 
+
+        for coin in coins : 
+            for i in range(coin , amount+1) : 
+                dp[i] = dp[i] + dp[i-coin]
+        
+        return dp[amount]
+
