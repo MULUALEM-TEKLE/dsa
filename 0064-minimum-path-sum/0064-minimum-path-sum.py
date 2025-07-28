eafd59cb3d200b59566ecf7b7ac3e989ca85dba8
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-07-28 09:32:54 +0300

    Time: 19 ms (38.32%), Space: 20.2 MB (52.38%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -3,8 +3,10 @@ class Solution:
         rows , cols = len(grid) , len(grid[0])
         dp = [[float('inf')]*(cols+1) for _ in range(rows+1)]
         dp[1][1] = grid[0][0]
-        for r in range(1 , rows+1) : 
-            for c in range(1 , cols+1) : 
+
+        for r in range(1 , len(dp)) : 
+            for c in range(1 , len(dp[0])) : 
                 if r == c == 1 : continue 
-                dp[r][c] = min(dp[r-1][c] , dp[r][c-1]) + grid[r-1][c-1]
+                dp[r][c] = grid[r-1][c-1] + min(dp[r-1][c] , dp[r][c-1])
+        
         return dp[rows][cols]
