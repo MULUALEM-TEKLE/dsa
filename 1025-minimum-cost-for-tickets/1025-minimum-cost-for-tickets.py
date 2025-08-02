d20824cd58486b15d4ca693cf8e9784033cb50fb
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-08-02 08:08:56 +0300

    Time: 28 ms (5.71%), Space: 18.4 MB (13%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -1,46 +1,25 @@
-'''
-You have planned some train traveling one year in advance. The days of the year in which you will travel are given as an integer array days. Each day is an integer from 1 to 365.
-
-Train tickets are sold in three different ways:
-
-a 1-day pass is sold for costs[0] dollars,
-a 7-day pass is sold for costs[1] dollars, and
-a 30-day pass is sold for costs[2] dollars.
-The passes allow that many days of consecutive travel.
-durations = [1 , 7 , 30] -> day the ticket bought inclusive 
-
-For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2, 3, 4, 5, 6, 7, and 8.
-Return the minimum number of dollars you need to travel every day in the given list of days.
--> minizing total cost
-
- 
-
-Example 1:
-
-Input: days = [1,4,6,7,8,20], costs = [2,7,15]
-Output: 11
-'''
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         durations = [1 , 7 , 30]
 
-        def find(next_day  ) : 
-            for index,day in enumerate(days) : 
-                if day >= next_day : return index
+        def find_next_day(next_valid_day) : 
+            for index, day in enumerate(days) : 
+                if day >= next_valid_day : return index
             return len(days)
 
         @cache
-        def solve(day) : 
-            if day >= len(days) : return 0
-            mincost = float('inf')
-            for index,cost in enumerate(costs) : 
-                next_day = days[day] + durations[index] - 1
+        def solve(i) : 
+            if i >= len(days) : return 0
 
-                next_day_index = find(next_day+1 )
-                print(next_day_index)
+            min_cost = float('inf')
 
-                mincost = min(mincost , solve(next_day_index) + cost)
+            for index , cost in enumerate(costs) : 
+                next_valid_day = days[i] + durations[index] - 1
 
-            return mincost
+                next_valid_day_index = find_next_day(next_valid_day+1)
+
+                min_cost = min(min_cost , cost + solve(next_valid_day_index))
+            
+            return min_cost
         
         return solve(0)
