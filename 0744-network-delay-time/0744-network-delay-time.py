3be1b3b059d9c68dc2103821feb795a1f13c9f4e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-08-13 11:43:27 +0300

    Time: 349 ms (99.72%), Space: 19.7 MB (53.83%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -7,22 +7,22 @@ class Solution:
             adj[u].append((v , w))
         
         pq = [(0 , k)]
-        distance = {node:float('inf') for node in range(1 , n+1)}
-        distance[k] = 0
+        times = {node : float('inf') for node in range(1,  n+1)}
 
         while pq : 
             w , node = heapq.heappop(pq)
-
-            if w > distance[node] : 
+            if w > times[node] : 
                 continue
 
-            for neighbor , weight in adj[node] : 
-                new_time = w + weight
-                if new_time < distance[neighbor] :
-                    distance[neighbor] = new_time 
-                    heapq.heappush(pq , (new_time , neighbor))
+            times[node] = w  
 
-        res = max(distance.values())
+            for neighbor , weight in adj[node] : 
+                new_time = weight + w 
+                if new_time < times[neighbor] : 
+                    times[neighbor] = new_time 
+                    heapq.heappush(pq , (new_time, neighbor))
+
+        res = max(times.values())
 
         return res if res != float('inf') else -1
         
