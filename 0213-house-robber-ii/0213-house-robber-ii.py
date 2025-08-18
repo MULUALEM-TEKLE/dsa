84488de2b122c5e52712a1379d547301b2d70aab
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-08-18 12:39:58 +0300

    Time: 0 ms (100%), Space: 17.9 MB (38.52%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -2,13 +2,13 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         nums = tuple(nums)
         @cache
-        def steal(sub , i) : 
+        def steal(nums , i) : 
             if i == 0 : 
-                return sub[0] 
+                return nums[0] 
             if i == 1 : 
-                return max(sub[0] , sub[1])
+                return max(nums[0] , nums[1])
             
-            return max(sub[i] + steal(sub , i-2) ,steal(sub , i-1))
+            return max(nums[i] + steal(nums , i-2) ,steal(nums , i-1))
         
         N = len(nums)
 
@@ -17,7 +17,10 @@ class Solution:
         if N == 2 : 
             return max(nums)
         
-        print(nums[1:])
-        print(nums[:-1])
+        tmp = nums 
+        nums = tmp[1:]
+        loot_without_first_house = steal(nums , N-2)
+        nums = tmp[:-1]
+        loot_without_last_house = steal(nums , N-2)
 
-        return max(steal(nums[1:] , N-2) , steal(nums[:-1] , N-2))        
+        return max(loot_without_first_house , loot_without_last_house )        
