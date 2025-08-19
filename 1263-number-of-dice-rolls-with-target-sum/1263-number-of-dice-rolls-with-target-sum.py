96ca1619464dbdd7ffc9c018307de908384b9efb
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-08-19 11:15:52 +0300

    Time: 315 ms (49.41%), Space: 23.7 MB (17.56%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
@@ -1,17 +1,20 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        MOD = ((10**9) + 7)
-        @cache
-        def roll(i , trgt) : 
-            if i == n and trgt == 0 : 
-                return 1
+        MOD = (10**9)+7
+        memo = {}
+        def roll(i , cur) : 
+            if (i , cur) in memo : return memo[(i , cur)]
+            if i == n : 
+                return 1 if cur == target else 0
+            
+            ways = 0 
 
-            if i >= n  : return 0
-
-            ways = 0
             for face in range(1 , k+1) : 
-                if trgt - face < 0 : break
-                ways += roll(i+1 , trgt - face)
-            return ways 
-        
-        return roll(0 , target) % MOD
+                if cur + face > target : break
+                ways += roll(i+1 , cur + face)
+            
+            memo[(i , cur)] = ways
+
+            return ways % MOD
+
+        return roll(0 , 0) 
