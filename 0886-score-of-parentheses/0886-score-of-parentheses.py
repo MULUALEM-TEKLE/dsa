4edd543da276712a9933653caec8916d91060d6d
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-08-19 11:31:46 +0300

    Time: 0 ms (100%), Space: 17.7 MB (44.65%) - LeetHub

--- 0886-score-of-parentheses/0886-score-of-parentheses.py
+++ 0886-score-of-parentheses/0886-score-of-parentheses.py
@@ -1,33 +1,15 @@
-'''
-Given a balanced parentheses string s, return the score of the string.
-
-The score of a balanced parentheses string is based on the following rule:
-
-"()" has score 1.
-AB has score A + B, where A and B are balanced parentheses strings.
--> s = "()()" score 2 we just add 1 and 1
-(A) has score 2 * A, where A is a balanced parentheses string.
--> s = "(())" score 2 because the inner parentheses has a score of 1 and the outer multiplies it by 2
--> s = "((()))" score 4 just multiply the above by 2
-approach : 
--> use stack
-    -> [0] first element will always be an opening, so for opening parentheses we just store 0
-    -> when we get a ')' we pop the top of the stack add 1 to the top of the stack
-        -> this work only for basic cases, what if s = "((()))" our approach will give [3] which is wrong
-        -> after poping the top if its 1 that means we have a nested parentheses so we have to go 
-        with the 3rd rule, 
-            -> after popping we have to either add 1 or 2 * the popped element
-            -> the maximum of two, if we have just an opened parentheses at the top it will be 1, 
-            if we just get a nested one it will be 2 
-'''
-
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        stack = [0]
+        res = []
+        cur = 0
+
         for c in s : 
-            if c == '(' : 
-                stack.append(0)
+            if c == "(" : 
+                res.append(cur)
+                cur = 0
             else : 
-                top = stack.pop()
-                stack[-1] += max(2*top , 1)
-        return stack[-1]
+                top = res.pop()
+                cur = top +  max(1 , cur*2)
+                
+        
+        return cur
