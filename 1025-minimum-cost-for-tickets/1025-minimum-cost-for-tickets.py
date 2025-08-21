431a58740c67c4cc410b3b6f9a1dfad6904e724c
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-08-21 23:57:25 +0300

    Time: 11 ms (18.58%), Space: 18.2 MB (24.2%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -1,10 +1,10 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         durations = [1 , 7 , 30]
-        def find(target_day) : 
-            for index,day in enumerate(days) :
-                if day >= target_day : 
-                    return index
+        def find(start , target_day) : 
+            for i in range(start ,len(days)) :
+                if days[i] >= target_day : 
+                    return i
             return len(days) 
         @cache
         def solve(i) : 
@@ -15,7 +15,7 @@ class Solution:
             for duration,cost in enumerate(costs) : 
                 ticket_duration = days[i]+durations[duration]-1
 
-                next_index = find(ticket_duration+1)
+                next_index = find(i , ticket_duration+1)
 
                 minm =min(minm,  cost + solve(next_index))
             return minm
