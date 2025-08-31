c71c7de1cd71d337890e78a83fa783a1e4a7aabf
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-08-31 11:55:39 +0300

    Time: 6 ms (2.58%), Space: 18.2 MB (10.37%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -2,17 +2,17 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         grid = obstacleGrid
         rows , cols = len(grid) , len(grid[0])
-        memo = [[False] * cols for _ in range(rows)]
-        visited = set()
-        def dfs(r , c):
-            if r not in range(rows) or c not in range(cols) or (r , c) in visited or grid[r][c] == 1: 
+
+        if grid[0][0] == 1 or grid[rows-1][cols-1] == 1 : 
+            return 0
+        
+        @cache
+        def dfs(r , c) : 
+            if r not in range(rows) or c not in range(cols) or grid[r][c] == 1 : 
                 return 0
-            if memo[r][c] : return memo[r][c]
-            if r == rows - 1 and c == cols - 1 : 
-                return 1 
-            visited.add((r , c))
-            res = dfs(r+1 , c) + dfs(r , c+1)
-            visited.remove((r , c))
-            memo[r][c] = res
-            return res
+            if r == rows-1 and c == cols-1 : 
+                return 1
+
+            return dfs(r+1 , c) + dfs(r , c+1) 
+        
         return dfs(0 , 0)
