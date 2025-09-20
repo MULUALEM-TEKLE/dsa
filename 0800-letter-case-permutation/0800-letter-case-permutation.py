959b83cfc92ec1cb625038f2a130b47b9cbcf19f
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-09-20 20:07:17 +0300

    Time: 7 ms (49.63%), Space: 19.2 MB (29.96%) - LeetHub

--- 0800-letter-case-permutation/0800-letter-case-permutation.py
+++ 0800-letter-case-permutation/0800-letter-case-permutation.py
@@ -2,19 +2,19 @@ class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         res = []
 
-        def backtrack(i , cur) : 
+        def dfs(i , cur) : 
             if i == len(s) : 
                 res.append(cur)
                 return 
-            
             if s[i].isalpha() : 
-                backtrack(i+1 , cur+s[i].upper())
-                backtrack(i+1 , cur+s[i].lower())
-            else : 
-                backtrack(i+1 , cur+s[i])
-        
-        backtrack(0 , "")
-
+                cur += s[i].lower()
+                dfs(i+1 , cur)
+                cur = cur[:-1]
+                cur += s[i].upper()
+                dfs(i+1 , cur)
+            elif s[i].isdigit() : 
+                cur += s[i]
+                dfs(i+1 , cur)
+                cur = cur[:-1]
+        dfs(0 , "")
         return res
-
-            
