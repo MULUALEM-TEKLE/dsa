3152427075f3cd2ad91cfba41f6c8a4a4f1c7892
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-04 22:55:06 +0300

    Time: 195 ms (76.68%), Space: 23.3 MB (24.76%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.py
@@ -2,10 +2,8 @@ class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         MOD = (10**9) + 7
 
-        memo = {}
+        @cache
         def roll(i , cur_sum) : 
-            # base case 
-            if (i , cur_sum) in memo : return memo[(i , cur_sum)]
             if i == n : 
                 return 1 if cur_sum == 0 else 0
 
@@ -16,7 +14,6 @@ class Solution:
                 if diff < 0 : break
                 ways += roll(i+1 , diff)
             
-            memo[(i , cur_sum)] = ways 
-            return memo[(i , cur_sum)]
+            return ways 
         
         return roll(0 , target) % MOD
