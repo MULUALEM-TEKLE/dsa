dd76dc904cdacee97c4f7e21178c468b155ce8bf
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-06 21:56:02 +0300

    Time: 45 ms (44.66%), Space: 18.9 MB (21.94%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,42 +1,42 @@
 class TrieNode : 
     def __init__(self) : 
         self.children = {}
-        self.eow = False
-
+        self.eow = False 
+        self.val = ""
+    
 class Trie : 
     def __init__(self) : 
         self.root = TrieNode()
-        self.root.eow = True
     
-    def insert(self, word) : 
-        cur = self.root
+    def insert(self, word) :
+        cur = self.root 
         for c in word : 
             if c not in cur.children : 
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
         cur.eow = True 
+        cur.val = word
     
-    def search(self, word) : 
-        cur = self.root
-        for c in word : 
-            if c not in cur.children : return False 
-            cur = cur.children[cur]
-        return cur.word 
+    def bfs(self) : 
+        q = deque([self.root])
+        res = ""
+        while q : 
+            node = q.popleft()
+            for child in node.children.values() : 
+                if child.eow : 
+                    if len(child.val) > len(res) : 
+                        res = child.val
+                    q.append(child)
+        return res
+        
+
 class Solution:
     def longestWord(self, words: List[str]) -> str:
+        words.sort()
+
         trie = Trie()
+
         for w in words : 
             trie.insert(w)
         
-        cand , l = [] , 0
-        explore = deque([(trie.root , [''])])
-        while explore : 
-            node , path = explore.pop()
-            if not node.eow : continue 
-            if len(path) > l : 
-                cand = path
-                l = len(path)
-            for c in reversed(string.ascii_lowercase) : 
-                if c in node.children : 
-                    explore.append((node.children[c] , path+[c]))
-        return ''.join(cand)
+        return trie.bfs()
