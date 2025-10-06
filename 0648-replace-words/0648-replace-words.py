2450b957992da72400536c524820c6e218299bdf
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-06 21:39:53 +0300

    Time: 47 ms (83.45%), Space: 34.4 MB (35.59%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -2,41 +2,43 @@ class TrieNode :
     def __init__(self) : 
         self.children = {}
         self.eow = False
-
 class Trie : 
     def __init__(self) : 
         self.root = TrieNode()
     
-    def insert(self, word) : 
-        cur = self.root
+    def insert(self , word) : 
+        cur = self.root 
+
         for c in word : 
             if c not in cur.children : 
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
-        cur.eow = True 
+
+        cur.eow = True
     
-    def search(self, word) : 
-        cur = self.root
+    def search(self , word) : 
+        cur = self.root 
         path = []
         for c in word : 
             if c not in cur.children : return None
             cur = cur.children[c]
             path.append(c)
-            if cur.eow : 
-                return "".join(path)
+            if cur.eow : return "".join(path)
         return None
 
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         trie = Trie()
+
         for word in dictionary : 
             trie.insert(word)
         
         sentence = sentence.split(" ")
         
-        for i,w in enumerate(sentence) : 
-            found = trie.search(w)
-            if found: 
-                sentence[i] = found
+        for index, item in enumerate(sentence) : 
+            root = trie.search(item)
 
+            if root : 
+                sentence[index] = root
+        
         return " ".join(sentence)
