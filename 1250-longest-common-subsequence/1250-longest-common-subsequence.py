06e23430299ee6cac22c87708d3910ddbd94a738
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-07 15:00:36 +0300

    Time: 1071 ms (19.38%), Space: 310.5 MB (6.96%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -3,13 +3,14 @@ class Solution:
         m , n = len(text1) , len(text2)
 
         @cache
-        def solve(i ,j) : 
+        def solve(i , j) : 
             if i == m or j == n : 
                 return 0
             
             if text1[i] == text2[j] : 
                 return 1 + solve(i+1 , j+1)
             else : 
-                return max(solve(i+1 , j) , solve(i , j+1))
-        
+                return max(solve(i+1 , j) ,  solve(i , j+1))
+            
         return solve(0 , 0)
+            
