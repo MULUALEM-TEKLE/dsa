1223dfe009a9a4e67613babbd204cdf03176b1fa
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-07 11:12:22 +0300

    Time: 300 ms (12.79%), Space: 29.8 MB (5.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,38 +1,45 @@
+class UF : 
+    def __init__(self , n) : 
+        self.par = [i for i in range(n)]
+        self.size = n
+    
+    def find(self , n) :
+        p = self.par[n]
+        while p != self.par[p] : 
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+    
+    def union(self, n2 , n1) :
+        print(f"n1 = {n1} and n2 = {n2}") 
+        p1 , p2 = self.find(n1) , self.find(n2)
+        if p1 != p2 : 
+            self.par[p2] = p1
+            self.size -= 1 
+           
+            
+
+        
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid) , len(grid[0])
 
-        visited = set()
-        def bfs(r , c) : 
-            q = deque()
-            q.append((r , c))
-            visited.add((r , c))
-
-            while q :
-                row , col = q.popleft()
-                directions = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
-                for dr , dc in directions : 
-                    nr , nc = row + dr , col + dc 
-                    if nr in range(rows) and nc in range(cols) and grid[nr][nc] == "1" and (nr, nc) not in visited : 
-                        q.append((nr , nc))
-                        visited.add((nr , nc))
-
-        def dfs(r , c) : 
-            if r not in range(rows) or c not in range(cols) or grid[r][c] != "1" or (r , c) in visited : 
-                return 
-
-            visited.add((r , c))
-
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r , c+1)
-            dfs(r , c-1)
-        
-        res = 0
+        ones_count = 0 
+        d = defaultdict(int)
         for r in range(rows) : 
             for c in range(cols) : 
-                if grid[r][c] == "1" and (r , c) not in visited : 
-                    dfs(r , c)
-                    res += 1 
+                if grid[r][c] == "1" : 
+                    d[(r , c)] = ones_count
+                    ones_count += 1 
         
-        return res
+        uf = UF(ones_count)
+
+        for r in range(rows) : 
+            for c in range(cols) : 
+                if grid[r][c] == "1" : 
+                    if r > 0 and grid[r-1][c] == "1":
+                        uf.union(d[(r , c)] ,d[(r-1 , c)] ) 
+                    if c > 0 and grid[r][c-1] == "1" : 
+                        uf.union(d[(r , c)] ,d[(r , c-1)] )
+
+        return uf.size 
