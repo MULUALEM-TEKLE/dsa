3118ff4088dda71cc7ffe9412c9451d806f7d011
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-07 10:39:32 +0300

    Time: 316 ms (9.41%), Space: 25.6 MB (17.9%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows , cols = len(grid) , len(grid[0])
+        rows, cols = len(grid) , len(grid[0])
+
         visited = set()
-
-        def dfs(r, c) : 
-            # base case 
-            if r < 0 or c < 0 or r == rows or c == cols or grid[r][c] == "0" or (r,c) in visited : 
-                return 0
-
-            # recursive case 
+        def bfs(r , c) : 
+            q = deque()
+            q.append((r , c))
             visited.add((r , c))
-            
-            dfs(r+1 , c) 
-            dfs(r-1 , c) 
-            dfs(r , c+1) 
-            dfs(r , c-1)
 
-            return 1
+            while q :
+                row , col = q.popleft()
+                directions = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
+                for dr , dc in directions : 
+                    nr , nc = row + dr , col + dc 
+                    if nr in range(rows) and nc in range(cols) and grid[nr][nc] == "1" and (nr, nc) not in visited : 
+                        q.append((nr , nc))
+                        visited.add((nr , nc))
 
-        count = 0
+        
+        res = 0
         for r in range(rows) : 
             for c in range(cols) : 
-                if grid[r][c] == "1" and (r,c) not in visited : 
-                    count += dfs(r , c)
-        return count
+                if grid[r][c] == "1" and (r , c) not in visited : 
+                    bfs(r , c)
+                    res += 1 
+        
+        return res
