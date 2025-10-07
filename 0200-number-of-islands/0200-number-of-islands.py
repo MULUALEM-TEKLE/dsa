f72b9f65eae272608543d7a6e9781b31f6c3b304
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-07 10:42:50 +0300

    Time: 317 ms (9.17%), Space: 29 MB (7.8%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -17,12 +17,22 @@ class Solution:
                         q.append((nr , nc))
                         visited.add((nr , nc))
 
+        def dfs(r , c) : 
+            if r not in range(rows) or c not in range(cols) or grid[r][c] != "1" or (r , c) in visited : 
+                return 
+
+            visited.add((r , c))
+
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r , c+1)
+            dfs(r , c-1)
         
         res = 0
         for r in range(rows) : 
             for c in range(cols) : 
                 if grid[r][c] == "1" and (r , c) not in visited : 
-                    bfs(r , c)
+                    dfs(r , c)
                     res += 1 
         
         return res
