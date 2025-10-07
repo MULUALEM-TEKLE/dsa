38a621f9a8b9abb9e9b3ab404b5f58a7ea7e7456
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-07 15:27:58 +0300

    Time: 1738 ms (8.28%), Space: 543.3 MB (5.43%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -4,17 +4,14 @@ class Solution:
 
         n = len(s)
 
-        memo = {}
+        @cache
         def solve(i , j) : 
-            if (i , j) in memo : return memo[(i,j)]
             if i == n or j == n : 
                 return 0 
             
             if s[i] == r[j] : 
-                memo[(i,j)] = 1 + solve(i+1, j+1)
-                return memo[(i,j)] 
+                return 1 + solve(i+1, j+1)
             else : 
-                memo[(i,j)] = max(solve(i , j+1) , solve(i+1 , j))
-                return memo[(i,j)]
+                return max(solve(i , j+1) , solve(i+1 , j))
         
         return solve(0 , 0)
