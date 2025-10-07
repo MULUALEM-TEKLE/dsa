f7d4fff64e07b6d095d80a329f7decfe157dce03
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-07 09:26:16 +0300

    Time: 12 ms (38.41%), Space: 17.9 MB (17.2%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.py
@@ -1,19 +1,12 @@
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
-        def isPrefixAndSuffix(str1, str2) :
-            # print(f'checking {str1} and {str2}')
-            # if str2[:len(str1)] == str1 and str2[:-(len(str1)-1)] == str1 : 
-            # print(f"{str2[:len(str1)]} == {str1} and {str2[:-(len(str1))]} == {str1}")
-            return str2[:len(str1)] == str1 and  str2[-(len(str1)):] == str1
-
         count = 0 
         for i in range(len(words)) : 
-            for j in range(i+1 ,len(words)) : 
-                if len(words[i]) > len(words[j]) : continue
-                if isPrefixAndSuffix(words[i], words[j]) : count += 1
+            for j in range(i+1 ,len(words)) :
+                w1 = words[i]
+                w2 = words[j] 
+                L = len(w1)
+                if len(w1) > len(w2) : continue
+                if w2.startswith(w1) and w2.endswith(w1) : count += 1
         
         return count 
-
-        # a = "a"
-        # aab = "aab"
-        # print(aab[-1:])
