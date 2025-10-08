0df101093326703bae82ab6394d2335031dc83e1
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-08 11:16:17 +0300

    Time: 920 ms (31.08%), Space: 32.9 MB (19.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,19 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # coins.sort()
+        coins.sort()
         @cache
         def find_ways(cur) : 
             if cur == 0 : 
                 return 0
-            
-            if cur < 0 : 
-                return float('inf')
            
             minm = float('inf')
             for coin in coins : 
                 diff = cur - coin
-                # if diff < 0 : 
-                #     break
+                if diff < 0 : 
+                    break
                 minm = min(minm , 1+find_ways(diff))
             return minm 
         
