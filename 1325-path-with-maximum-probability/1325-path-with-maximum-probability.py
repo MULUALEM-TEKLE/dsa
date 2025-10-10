55b83ff6cc5c788154449c7393c48bb252e73d8c
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-10 09:35:24 +0300

    Time: 125 ms (33.42%), Space: 29.7 MB (15.14%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -1,26 +1,27 @@
+from heapq import heappop , heappush
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
         adj = defaultdict(list)
 
         for i in range(len(edges)) : 
             src , dst = edges[i]
-            adj[src].append((dst , succProb[i] ))
-            adj[dst].append((src , succProb[i] ))
+            prob = succProb[i]
+            adj[src].append([dst , prob])
+            adj[dst].append([src , prob])
         
+        pq = [(-1.0 , start_node)]
         visited = set()
-        maxHeap = [(-1 , start_node)]
-
-        while maxHeap : 
-            prob1 , node1 = heapq.heappop(maxHeap)
-            visited.add(node1)
+        while pq : 
+            w , node = heappop(pq)
+            print(f"w = {w} and node = {node}")
+            if node in visited : 
+                continue 
+            if node == end_node : 
+                return w * -1.0
             
-            if node1 == end_node : 
-                return prob1 * -1
+            visited.add(node)
             
-            
-            for node2,prob2 in adj[node1] : 
-                if node2 not in visited : 
-                    heapq.heappush(maxHeap , (prob1*prob2 , node2))
-
-        # return highest[end_node] * -1 if end_node in highest else 0
+            for neighbor , prob in adj[node] : 
+                if neighbor not in visited : 
+                    heappush(pq , (w * prob , neighbor))
         return 0
