c7fca0d9761224fd27c67c58782db12b55cf7caf
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-12 12:35:16 +0300

    Time: 507 ms (5.08%), Space: 20.9 MB (5.39%) - LeetHub

---
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def get_gcd(n1 , n2 ) : 
+            print(f"getting gcd of {n1} and {n2}")
+            if n1 == n2 : 
+                return n1 
+            if n1 == 1  or n2 == 1 : 
+                return 1
+            gcd = n1 if n1 < n2 else n2 
+            for i in range(gcd , 0 , -1) : 
+                if n1 % i == n2 % i == 0 : 
+                    print(f"the gcd is {i}")
+                    return i
+        
+        dummy = ListNode(0)
+        cur = dummy
+
+        left = head 
+
+        while  left.next : 
+            gcd = get_gcd(left.val , left.next.val)
+            cur.next = ListNode(left.val)
+            cur = cur.next 
+            cur.next = ListNode(gcd)
+            # cur = cur.next 
+            # cur.next = ListNode(left.next.val)
+            left = left.next
+            cur = cur.next
+        cur.next = ListNode(left.val)
+        cur = dummy 
+        s = ''
+        while cur : 
+            s += f"-> {cur.val}"
+            cur = cur.next 
+        
+        print(s)
+
+        return dummy.next
+            
