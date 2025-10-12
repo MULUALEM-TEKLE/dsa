4b79ac0dd783e9326b52eeb54445a398c382bb98
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-12 11:29:22 +0300

    Time: 397 ms (16.98%), Space: 18.1 MB (9%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,25 +1,25 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        res_up = float('inf')
-        res_down = float('-inf')
+       
         n = len(nums)
         nums.sort()
+        closest_sum = float("inf")
 
         for i,num in enumerate(nums) : 
-            # print(f"checking for {i} index num {num}")
+            # if i > 0 and i < n-1 and nums[i] == nums[i+1] : continue
             left , right = i + 1 , n - 1
-            # print(f"now left is {left} and right is {right}")
-            while left < right : 
-                if num + nums[right] + nums[left] > target : 
-                    res_up = min(res_up , num + nums[right] + nums[left])
-                    right -= 1 
-                elif num + nums[right] + nums[left] < target : 
-                    res_down = max(res_down , num + nums[right] + nums[left])              
-                    # print(f"maximum res_down found so far {res_down}")
-                    left += 1 
-                else : 
-                    return num + nums[right] + nums[left]
 
-        if abs(target - res_up) < abs(target-res_down) : 
-            return res_up 
-        return res_down 
+            while left < right : 
+                cur_sum = num + nums[right] + nums[left] 
+                
+                if abs(cur_sum-target) < abs(closest_sum-target) : 
+                    closest_sum = cur_sum
+                
+                if cur_sum == target : 
+                    return cur_sum
+                elif cur_sum < target : 
+                    left += 1
+                else : 
+                    right -= 1 
+
+        return closest_sum 
