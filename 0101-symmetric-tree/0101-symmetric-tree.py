3cb848cba45b2007a1f62a494ad6e30669d88140
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-15 21:37:41 +0300

    Time: 0 ms (100%), Space: 18.1 MB (12.6%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,14 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+def reverse(root) : 
+    if not root : 
+        return 
+    root.left , root.right = root.right , root.left
+    reverse(root.left)
+    reverse(root.right)
+
+
+def same(p , q) : 
+    if not q and not p : 
+        return True
+    if not q or not p or q.val != p.val : 
+        return False 
+    # if p.val == q.val : 
+    #     return True 
+    
+    return same(p.left , q.left) and same(p.right , q.right)
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def dfs(left , right) : 
-            if not left and not right : 
-                return True 
-            if not left or not right : 
-                return False 
-            
-            return left.val == right.val and dfs(left.left , right.right) and dfs(left.right , right.left)
-        
-        return dfs(root.left , root.right)
+        reverse(root.right)
+        print(root.right)
+        return same(root.left , root.right)
