912d918e76bdf89303da4ca8810e71a21f3a3608
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-18 12:56:07 +0300

    Time: 49 ms (21.19%), Space: 18.3 MB (6.5%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -7,7 +7,11 @@ class Solution:
             if cur == 0 : 
                 return 1 
             res = 0 
-            return sum(dfs(cur-num) for num in nums)
+            for num in nums : 
+                res += dfs(cur - num)
+            
+            return res
+            # return sum(dfs(cur-num) for num in nums)
         
         return dfs(target)
 
