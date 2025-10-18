45d33de36c62c6db8f94b5db835ad72de12597bd
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-18 18:07:51 +0300

    Time: 139 ms (56.69%), Space: 45.2 MB (8.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,11 +1,10 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         @cache
-        def explore(i , total) : 
-            # base cases 
+        def explore(i , cur) : 
             if i == len(nums) : 
-                return 1 if total == target else 0
-
-            return explore(i+1 , total + nums[i]) + explore(i+1 , total - nums[i])
-
+                return 1 if cur == target else 0
+            
+            return explore(i+1 , cur - nums[i]) + explore(i+1 , cur + nums[i])
+        
         return explore(0 , 0)
