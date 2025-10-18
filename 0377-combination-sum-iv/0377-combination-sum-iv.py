fe0ae07fa9783d022ac9655e475a24f0035de670
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-18 12:56:50 +0300

    Time: 39 ms (79.71%), Space: 18 MB (9.97%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,15 +1,16 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
+        nums.sort()
         @cache
         def dfs(cur) : 
-            if cur < 0 : 
-                return 0
+            # if cur < 0 : 
+            #     return 0
             if cur == 0 : 
                 return 1 
             res = 0 
             for num in nums : 
                 if cur-num < 0 : 
-                    continue
+                    break
                 res += dfs(cur - num)
             
             return res
