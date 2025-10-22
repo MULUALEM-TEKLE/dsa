84498bcc97c2a59110dcabb701826443a222473e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-23 00:29:31 +0300

    Time: 130 ms (59.98%), Space: 45.2 MB (8%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -4,7 +4,16 @@ class Solution:
         def explore(i , cur) : 
             if i == len(nums) : 
                 return 1 if cur == target else 0
-            
-            return explore(i+1 , cur - nums[i]) + explore(i+1 , cur + nums[i])
+            return explore(i+1 , cur+nums[i]) + explore(i+1 , cur-nums[i])
         
         return explore(0 , 0)
+
+        # space : dp = [[0]*(target+1) for _ in range(len(nums))]
+        # base case : last row : 1 for c == target 
+        # recurrence : dp[i-1][c+nums[i]] + dp[i-1][c-nums[i]]
+
+        # dp = [[0]*(target+1) for _ in range(len(nums))]
+
+        # for i in range(len(nums)) : 
+        #     for c in range(target+1) : 
+        #         plus = dp[i][]
