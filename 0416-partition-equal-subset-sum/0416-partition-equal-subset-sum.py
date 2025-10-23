ed2aae0eb889d15a54a786a8281e1d22028f4281
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-23 13:10:56 +0300

    Time: 985 ms (40.31%), Space: 33.4 MB (38.89%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -21,15 +21,36 @@ class Solution:
         # return target in dp
 
 
-        @cache
-        def explore(i , cur) : 
-            if cur > target : 
-                return False
-            if cur == target : 
-                return True 
-            if i == len(nums) : 
-                return False
+        # @cache
+        # def explore(i , cur) : 
+        #     if cur > target : 
+        #         return False
+        #     if cur == target : 
+        #         return True 
+        #     if i == len(nums) : 
+        #         return False
             
-            return explore(i+1 , cur+nums[i]) or explore(i+1 , cur)
+        #     return explore(i+1 , cur+nums[i]) or explore(i+1 , cur)
         
-        return explore(0 , 0)
+        # return explore(0 , 0)
+
+        n = len(nums)
+        dp = [[0]*(target+1) for _ in range(n+1)]
+
+        for i in range(n+1) : 
+            dp[i][0] = 1
+        
+        for r in range(1 , n+1) : 
+            num = nums[r-1]
+            for c in range(1 , target+1) : 
+                leave = dp[r-1][c]
+
+                take = 0
+                if c >= num : 
+                    take = dp[r-1][c-num]
+
+                dp[r][c] = take or leave
+            if dp[r][target] : return True 
+        
+
+        return bool(dp[n][target])
