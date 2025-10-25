11d647aa19b2adda29397bca5a4058f066966efc
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-25 10:44:41 +0300

    Time: 27 ms (45.25%), Space: 25.4 MB (9.93%) - LeetHub

--- 1130-last-stone-weight-ii/1130-last-stone-weight-ii.py
+++ 1130-last-stone-weight-ii/1130-last-stone-weight-ii.py
@@ -2,11 +2,15 @@ class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
         total = sum(stones)
         target = total//2
+        self.res = float('inf')
         @cache
         def explore(i , cur) : 
             if cur >= target or i == len(stones) : 
-                return abs(cur - (total - cur))
-            return min(explore(i + 1 , cur + stones[i]) , explore(i + 1 , cur ))
+                self.res = min(self.res , abs(cur - (total - cur)))
+                return 
+            explore(i + 1 , cur + stones[i]) 
+            explore(i + 1 , cur )
 
-        return explore(0 , 0)
+        explore(0 , 0)
+        return self.res
      
