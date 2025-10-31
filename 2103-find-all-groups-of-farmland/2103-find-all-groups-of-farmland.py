593fcd262088ad3fdc3276d26a340a2c44a7986e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-31 13:24:25 +0300

    Time: 542 ms (6.41%), Space: 37.5 MB (35.29%) - LeetHub

--- 2103-find-all-groups-of-farmland/2103-find-all-groups-of-farmland.py
+++ 2103-find-all-groups-of-farmland/2103-find-all-groups-of-farmland.py
@@ -1,30 +1,33 @@
 class Solution:
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
-        rows , cols = len(land) , len(land[0])
-        visited = set()
+        rows, cols = len(land) , len(land[0])
+        directions = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
+
         res = []
 
-        def bfs(r , c) : 
+        visited = set()
+
+        def bfs(r , c ) : 
+            start = [r , c]
             q = deque()
-            q.append((r , c))
+            q.append([r , c])
             visited.add((r , c))
 
-            location = [r , c]
-            end = None
             while q : 
                 row , col = q.popleft()
                 end = [row , col]
-                directions = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
-                for dr,dc in directions : 
-                    r , c = row+dr , col+dc
-                    if r in range(rows) and c in range(cols) and (r , c) not in visited and land[r][c] == 1 : 
-                        q.append((r , c))
-                        visited.add((r , c))
-            location.extend(end)
-            res.append(location)
+                for dr , dc in directions : 
+                    nr , nc = row + dr , col + dc
+                    if nr in range(rows) and nc in range(cols) and (nr , nc) not in visited and land[nr][nc] == 1 : 
+                        q.append((nr , nc))
+                        visited.add((nr , nc))
+
+            res.append(start+end)
+
         
-        for r in range(rows): 
-            for c in range(cols): 
-                if land[r][c] == 1 and (r , c) not in visited : 
+        for r in range(rows) : 
+            for c in range(cols) : 
+                if land[r][c] == 1 and (r,c) not in visited : 
                     bfs(r , c)
+        
         return res
