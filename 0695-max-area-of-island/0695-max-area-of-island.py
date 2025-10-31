171cc2b9116f88e59a16ac9dd8f06be71d60e526
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-10-31 14:26:19 +0300

    Time: 43 ms (9.98%), Space: 18.4 MB (52.85%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,28 +1,33 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows , cols = len(grid) , len(grid[0])
-        dxn = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
+        rows, cols = len(grid) , len(grid[0])
+        directions = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
+
         visited = set()
+        self.max_area = 0
 
         def bfs(r , c) : 
-            q = deque()
-            q.append((r,c))
-            visited.add((r,c))
-            island_area = 1
-            while q: 
-                row , col = q.popleft()
-                for dr,dc in dxn : 
-                    r , c = row + dr , col + dc
-                    if r in range(rows) and c in range(cols) and (r , c) not in visited and grid[r][c] == 1 : 
-                        q.append((r, c))
-                        visited.add((r , c))
-                        island_area += 1 
-            return island_area
-            
+            q = deque([(r , c)])
+            visited.add((r , c))
 
-        area = 0
+            area = 1
+
+            while q : 
+                row , col = q.popleft()
+                for dr , dc in directions : 
+                    nr , nc = row + dr , col + dc 
+                    if nr in range(rows) and nc in range(cols) and (nr , nc ) not in visited and grid[nr][nc] == 1 : 
+                        area += 1 
+                        visited.add((nr , nc))
+                        q.append((nr , nc))
+
+            self.max_area = max(self.max_area , area) 
+        
+
         for r in range(rows) : 
             for c in range(cols) : 
-                if grid[r][c] == 1 and (r , c) not in visited: 
-                    area = max(area , bfs(r , c))
-        return area
+                if grid[r][c] == 1 and (r , c) not in visited : 
+                    bfs(r , c)
+        
+
+        return self.max_area
