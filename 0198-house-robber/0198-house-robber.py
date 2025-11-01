b285cd49bf47f1ea4d018adb8482246dd5916d9a
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-11-01 22:08:03 +0300

    Time: 0 ms (100%), Space: 17.8 MB (54.1%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         @cache
-        def explore(i) : 
-            # base cases 
-            if i == 1 : 
-                return max(nums[0] , nums[1])
+        def steal(i) :
             if i == 0 : 
                 return nums[0]
-
-            return max(nums[i] + explore(i-2) , explore(i-1))
-        
-        return explore(len(nums)-1)
+            if i == 1 : 
+                return max(nums[0] , nums[1])
+            return max(nums[i]+steal(i-2) , steal(i-1))
+        return steal(len(nums)-1)
