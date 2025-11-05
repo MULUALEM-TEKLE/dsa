29a24aae69a5d51f866f7a3ec620d5dd6334f3a0
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-11-05 23:06:57 +0300

    Time: 18 ms (91.41%), Space: 17.9 MB (51.03%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,28 +1,24 @@
-from heapq import heappush , heappop
+from heapq import heappush, heappop
+
+
 class Solution:
     def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
         res = []
 
-        def process(arr) : 
+        def process(arr):
             cnt = Counter(arr)
-            print(cnt)
             pq = []
-            for key,val in cnt.items() : 
-                heappush(pq , [val , key])
-                if len(pq) > x : 
+            for key, val in cnt.items():
+                heappush(pq, [val, key])
+                if len(pq) > x:
                     heappop(pq)
-            # print(pq)
             summ = 0
-            for i in range(len(pq)) : 
-                summ += pq[i][0] * pq[i][1]
-            
+            for m,n in pq:
+                summ += m*n
+
             res.append(summ)
-            
 
-            
-        
-        for left in range(len(nums)-k+1) : 
-            process(nums[left:left+k])
-
+        for left in range(len(nums) - k + 1):
+            process(nums[left : left + k])
 
         return res
