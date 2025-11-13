4b57c9eede9c1e390a916d25dcbba9238ecf4b77
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2025-11-14 00:21:23 +0300

    Time: 39 ms (13.83%), Space: 19.6 MB (92.19%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -3,6 +3,5 @@ class Solution:
         nums.sort()
         total = 0 
         for i in range(1 , len(nums) , 2 ) : 
-            print(i)
             total += min(nums[i] , nums[i-1])
         return total
