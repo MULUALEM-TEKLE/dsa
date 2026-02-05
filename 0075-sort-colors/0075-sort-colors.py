abbd36998100dd7564eacd98369fb09cf1f775f4
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-05 12:31:14 +0300

    Time: 0 ms (100%), Space: 19.3 MB (34.74%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,23 +1,18 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        max_range = max(nums) + 1
-        counter = [0] * max_range
-
-        for num in nums :
-            counter[num] += 1
-        print(counter)
-        # nums.clear()
-        # for i in range(len(counter)):
-        #     nums.extend([i] * counter[i])
-        
-        # print(nums)
-        i = 0
-        for n in range(len(counter)):
-            for _ in range(counter[n]) : 
-                nums[i] = n
-                i +=1
-        
         """
         Do not return anything, modify nums in-place instead.
         """
+        max_range = max(nums)+1
+        counter = [0] * max_range
+
+        for num in nums : 
+            counter[num] += 1 
         
+
+        i = 0 
+        for n in range(len(counter)) : 
+            for _ in range(counter[n]) : 
+                nums[i] = n
+                i+= 1 
+            
