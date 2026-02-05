8015d0cdc7837581ac8e08085675ef1ba1bd400e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-05 16:08:34 +0300

    Time: 0 ms (100%), Space: 19.7 MB (20.96%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,27 +1,28 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        low , high = 0 , len(matrix) -1
-        while low <= high:
-            mid = (low + high)//2
-            if target == matrix[mid][0] or  matrix[mid][-1] == target :
-                return True
-            elif target > matrix[mid][0] and  matrix[mid][-1] > target : 
-                break
-            elif target < matrix[mid][0] : 
-                high = mid -1
-            else : 
-               low = mid + 1
+        row , col = len(matrix) , len(matrix[0])
+        low , high = 0 , row-1
+        location = -1
 
-        location = matrix[((high+ low)//2)]
-        low , high = 0 , len(location) -1
-        while low <= high:
-            mid = (low + high)//2
-            print(f"mid is now {mid}")
-            if target > location[mid] : 
-                low = mid + 1
-            elif target < location[mid] : 
-                high = mid -1
+        while low <= high : 
+            mid = (low+high)//2
+            if matrix[mid][0] > target : 
+                high = mid - 1 
             else : 
-               return True
+                low = mid + 1 
+                location = mid 
+        
+        low , high = 0 , col-1
 
-        return False
+        while low <= high : 
+            mid = (low+high)//2
+            if matrix[location][mid] < target :
+                low = mid + 1 
+            elif matrix[location][mid] > target : 
+                high = mid -1 
+            else : 
+                return True 
+        
+        return False 
+        
+        
