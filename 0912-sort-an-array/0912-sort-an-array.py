7123d385dde82822acaae8dca64f7b2e3bb1d688
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-05 12:13:19 +0300

    Time: 732 ms (28.15%), Space: 24.3 MB (94.01%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,42 +1,39 @@
+def merge(arr ,start , mid , end) : 
+    L = arr[start : mid + 1]
+    R = arr[mid + 1 : end + 1]
+    LP = RP = 0
+    AP = start 
+
+    while LP < len(L) and RP < len(R) : 
+        if R[RP] < L[LP] : 
+            arr[AP] = R[RP]
+            RP += 1 
+        else : 
+            arr[AP] = L[LP]
+            LP += 1 
+        AP += 1 
+    
+    while LP < len(L) : 
+        arr[AP] = L[LP]
+        LP += 1 
+        AP += 1 
+    while RP < len(R) : 
+        arr[AP] = R[RP]
+        RP += 1 
+        AP += 1 
+    
+def sort(arr , start , end) : 
+    if end - start + 1 <= 1 : 
+        return 
+    
+    mid = (start + end )//2
+
+    sort(arr , start , mid)
+    sort(arr , mid + 1 , end)
+
+    merge(arr , start , mid , end)
+
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge(start , mid , end) : 
-            L = nums[start : mid+1]
-            R = nums[mid+1 : end+1]
-            LP = RP = 0
-            NP = start
-
-            while LP < len(L) and RP < len(R) : 
-                if L[LP] < R[RP] : 
-                    nums[NP] = L[LP]
-                    LP += 1 
-                else : 
-                    nums[NP] = R[RP]
-                    RP += 1
-                NP += 1 
-            
-            while LP < len(L) : 
-                nums[NP] = L[LP]
-                LP += 1 
-                NP += 1 
-            
-            while RP < len(R) : 
-                nums[NP] = R[RP]
-                RP += 1 
-                NP += 1 
-
-        def m_sort(start , end) : 
-            if end - start + 1 <= 1 : 
-                return 
-            mid = (start+end) // 2 
-            m_sort(start , mid)
-            m_sort(mid+1 , end)
-
-            merge(start , mid , end)
-
-            return nums
-        
-        return m_sort(0 , len(nums))
-
-
-        
+        sort(nums , 0 , len(nums)-1)
+        return nums
