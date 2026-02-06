795d177c037e6b40da8dff917baf4010b277daae
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-06 11:11:19 +0300

    Time: 1457 ms (57.96%), Space: 358.1 MB (5.01%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.py
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -1,15 +1,16 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         table = defaultdict(int)
-        for i,s in enumerate(strs) : 
+
+        for i , s in enumerate(strs) : 
             table[i] = Counter(s)
         
         @cache
-        def lss(i , m_cur , n_cur) : 
-            if m_cur > m or n_cur > n  :
+        def lss(i, n_c , m_c) : 
+            if n_c > n or m_c > m : 
                 return -1
             if i == len(strs) : 
                 return 0
-            return max(lss(i+1 , m_cur , n_cur) , 
-            1+lss(i+1 , m_cur+table[i]['0'] , n_cur+table[i]['1'])) 
-        return lss(0 , 0 , 0)
+            return max(lss(i+1 , n_c , m_c) , 1 + lss(i+1 , n_c+table[i]['1'] , m_c+table[i]['0']))
+        
+        return lss(0 , 0, 0)
