f697414051ce1284c4a5ee5d6f9319c802521bbb
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-06 11:38:40 +0300

    Time: 43 ms (77.44%), Space: 20.5 MB (12.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,23 +2,19 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         m , n = len(s1) , len(s2)
 
-        if m + n != len(s3) : 
+        if m+n != len(s3) : 
             return False
+
         @cache
         def explore(i , j) : 
             if i == m and j == n : 
+                return True
+
+            k = i+j
+
+            if (i < m and s1[i] == s3[k] and explore(i+1 , j)) or (j < n and s2[j] == s3[k] and explore(i , j+1)) : 
                 return True 
             
-            k = i+j
-            
-            take_i = False
-            if i < m and s1[i] == s3[k] : 
-                take_i = explore(i+1 , j)
-            
-            take_j = False
-            if j < n and s2[j] == s3[k] : 
-                take_j = explore(i , j+1)
-            
-            return take_i or take_j
+            return False
         
         return explore(0 , 0)
