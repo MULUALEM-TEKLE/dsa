ceca2e059721e6d21b665326d2dea0c0c336b8c7
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-09 12:15:04 +0300

    Time: 12 ms (28.93%), Space: 33.9 MB (5.46%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -5,10 +5,22 @@ class Solution:
         for num in nums : 
             scores[num] += num
         
+        u_nums = sorted(scores.keys())
+        
         @cache
         def explore(i) : 
-            if i <= 0 : 
+            if i < 0 : 
                 return 0
-            return max(scores[i] + explore(i-2) , explore(i-1))
+            if i == 0 : 
+                return scores[u_nums[0]]
+            
+            skip = explore(i-1)
+
+            if u_nums[i] == u_nums[i-1] + 1 : 
+                take = scores[u_nums[i]] + explore(i-2)
+            else : 
+                take = scores[u_nums[i]] + explore(i-1)
+            
+            return max(take , skip)
         
-        return explore(max(nums))
+        return explore(len(u_nums)-1)
