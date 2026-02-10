1c5e4dd64ed2742d527b4a0f3b1582a330238d0e
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-10 09:16:00 +0300

    Time: 304 ms (7.01%), Space: 26.8 MB (21.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,45 +1,28 @@
-class UF : 
-    def __init__(self , n) : 
-        self.par = [i for i in range(n)]
-        self.size = n
-    
-    def find(self , n) :
-        p = self.par[n]
-        while p != self.par[p] : 
-            self.par[p] = self.par[self.par[p]]
-            p = self.par[p]
-        return p
-    
-    def union(self, n2 , n1) :
-        print(f"n1 = {n1} and n2 = {n2}") 
-        p1 , p2 = self.find(n1) , self.find(n2)
-        if p1 != p2 : 
-            self.par[p2] = p1
-            self.size -= 1 
-           
-            
-
-        
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid) , len(grid[0])
+        nei = [[0 , 1] , [0 , -1] , [1 , 0] , [-1 , 0]]
+        visited = set()
+        count = 0
 
-        ones_count = 0 
-        d = defaultdict(int)
-        for r in range(rows) : 
-            for c in range(cols) : 
-                if grid[r][c] == "1" : 
-                    d[(r , c)] = ones_count
-                    ones_count += 1 
+        def bfs(r , c ) : 
+            q = deque()
+            q.append((r , c))
+            visited.add((r , c))
+
+            while q : 
+                row , col = q.popleft()
+
+                for dr , dc in nei : 
+                    nr , nc = row+dr , col+dc
+                    if nr in range(rows) and nc in range(cols) and (nr , nc) not in visited and grid[nr][nc] == "1" : 
+                        q.append((nr, nc))
+                        visited.add((nr , nc))
         
-        uf = UF(ones_count)
-
         for r in range(rows) : 
             for c in range(cols) : 
-                if grid[r][c] == "1" : 
-                    if r > 0 and grid[r-1][c] == "1":
-                        uf.union(d[(r , c)] ,d[(r-1 , c)] ) 
-                    if c > 0 and grid[r][c-1] == "1" : 
-                        uf.union(d[(r , c)] ,d[(r , c-1)] )
-
-        return uf.size 
+                if (r , c) not in visited and grid[r][c] == "1" :
+                    bfs(r , c)
+                    count += 1 
+        
+        return count
