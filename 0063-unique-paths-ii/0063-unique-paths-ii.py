34fd1ddf09ec85b791d068837c0a0e896737ae23
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-11 07:38:34 +0300

    Time: 0 ms (100%), Space: 20.1 MB (5.17%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,16 +1,16 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m , n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0]*(n+1) for _ in range(m+1)]
-        if obstacleGrid[0][0] == 1 or obstacleGrid[m-1][n-1] == 1 : 
-            return 0
-        # dp[m-1][n-1] = 1
-        for i in range(m-1 , -1 , -1) : 
-            for j in range(n-1 , -1 , -1) : 
-                if i == m-1 and j == n-1 : 
-                    dp[i][j] = 1
-                    continue
-                if obstacleGrid[i][j] == 1 : continue
-                dp[i][j] = dp[i+1][j] + dp[i][j+1]
-        # print(dp)
-        return dp[0][0]
+        grid = obstacleGrid
+        rows , cols = len(grid) , len(grid[0])
+        if grid[0][0] == 1 or grid[rows-1][cols-1] == 1 : return 0
+
+        @cache
+        def explore(r , c) : 
+            if r not in range(rows) or c not in range(cols) or grid[r][c] == 1 : 
+                return 0
+            if r == rows-1 and c == cols-1 : 
+                return 1 
+            
+            return explore(r+1 , c ) + explore(r , c+1)
+        
+        return explore(0 , 0)
