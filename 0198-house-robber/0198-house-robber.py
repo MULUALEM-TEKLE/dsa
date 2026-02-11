f389e6ad1fd6126a2a6559d8e1e01bece303612f
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-11 07:28:35 +0300

    Time: 4 ms (2.54%), Space: 19.3 MB (40.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,10 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @cache
+        cache = defaultdict(int)
         def steal(i) :
+            if i in cache : return cache[i] 
             if i == 0 : 
                 return nums[0]
             if i == 1 : 
                 return max(nums[0] , nums[1])
-            return max(nums[i]+steal(i-2) , steal(i-1))
+            
+            cache[i] = max(steal(i-1) , nums[i]+steal(i-2))
+            return cache[i]
+        
         return steal(len(nums)-1)
