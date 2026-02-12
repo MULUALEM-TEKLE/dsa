acf0f913b29d29203ee8d5f38ac3d90bde40b198
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-12 20:46:46 +0300

    Time: 19 ms (14.47%), Space: 36.8 MB (5%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,26 +1,14 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         scores = defaultdict(int)
-
         for num in nums : 
             scores[num] += num
-        
-        u_nums = sorted(scores.keys())
-        
-        @cache
-        def explore(i) : 
-            if i < 0 : 
-                return 0
-            if i == 0 : 
-                return scores[u_nums[0]]
-            
-            skip = explore(i-1)
 
-            if u_nums[i] == u_nums[i-1] + 1 : 
-                take = scores[u_nums[i]] + explore(i-2)
-            else : 
-                take = scores[u_nums[i]] + explore(i-1)
+        @cache
+        def dfs(i) : 
+            if i <= 0 : 
+                return 0
             
-            return max(take , skip)
+            return max(dfs(i-1) , scores[i]+dfs(i-2))
         
-        return explore(len(u_nums)-1)
+        return dfs(max(nums))
