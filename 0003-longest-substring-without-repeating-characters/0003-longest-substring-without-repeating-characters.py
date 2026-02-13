9a79a6ae14103376e19d0ff665af04ea90331ebd
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-13 13:59:22 +0300

    Time: 11 ms (83.72%), Space: 19.5 MB (5.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,13 +4,13 @@ class Solution:
         if s == "" : return 0
         window.add(s[0])
         longest = 1
-        left = 0
+        left = 0 
 
         for right in range(1 , len(s)) : 
             while s[right] in window : 
                 window.remove(s[left])
                 left += 1 
-            window.add(s[right])
             longest = max(longest , right-left+1)
+            window.add(s[right])
         
         return longest
