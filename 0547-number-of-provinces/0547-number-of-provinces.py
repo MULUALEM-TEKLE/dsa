6e3ed09427a506c819c667d99dc4e1ceb290dd14
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-18 12:15:46 +0300

    Time: 63 ms (5.09%), Space: 21.2 MB (25.13%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,16 +1,17 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         grid = isConnected 
-        n = len(grid)
+        rows , cols = len(grid) , len(grid[0])
+        self.size = len(grid)
         edges = []
 
-        for r in range(n) : 
-            for c in range(n) : 
-                if r != c and grid[r][c] == 1 : edges.append([r , c])
+        for r in range(rows) : 
+            for c in range(cols) : 
+                if r != c and grid[r][c] == 1 : 
+                    edges.append([r , c])
         
-        self.size = n
-        par = [i for i in range(self.size)]
-        
+        par = [i for i in range(self.size+1)]
+
         def find(n) : 
             p = par[n]
             while p != par[p] : 
@@ -19,11 +20,13 @@ class Solution:
         
         def union(n1 , n2) : 
             p1 , p2 = find(n1) , find(n2)
-            if p1 != p2 : 
-                par[p1] = p2
-                self.size -= 1 
-        
+            if p1 == p2 : 
+                return 
+            par[p1] = p2 
+            self.size -= 1 
+
         for n1 , n2 in edges : 
             union(n1 , n2)
+        
+        return self.size
 
-        return self.size 
