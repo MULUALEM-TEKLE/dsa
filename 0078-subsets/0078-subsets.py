8b12b1af206033332eb63efcabb08dea2c65bba0
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-19 12:56:47 +0300

    Time: 0 ms (100%), Space: 19.4 MB (68.57%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -4,14 +4,16 @@ class Solution:
 
         sub = []
 
-        def dfs(i) : 
-            if i == len(nums) : 
+        def backtrack(i) : 
+            if i >= len(nums) : 
                 res.append(sub[:])
                 return
+
             sub.append(nums[i])
-            dfs(i+1)
+            backtrack(i+1)
             sub.pop()
-            dfs(i+1)
+            backtrack(i+1)
         
-        dfs(0)
-        return res
+        backtrack(0)
+        return res 
+            
