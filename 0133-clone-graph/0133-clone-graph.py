fb237bcb6cbaf76054acefa64fddef33f09771ad
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-02-27 15:31:33 +0300

    Time: 52 ms (37.7%), Space: 19.7 MB (76.47%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -12,19 +12,36 @@ class Solution:
         if not node : 
             return None
 
-        start = node 
-        o_to_n = {}
+        o2n = { }
+
+        def build(root) : 
+          
+            o2n[root] = Node(root.val)
+
+            for neighbor in root.neighbors : 
+                if neighbor not in o2n : 
+                   build(neighbor)
         
-        def dfs(node) : 
-            o_to_n[node] = Node(node.val)
-            for nei in node.neighbors :
-                if nei not in o_to_n : 
-                    dfs(nei)
+        # def copy(root) : 
+        #     if not root : 
+        #         return 
+
+        #     if root in o2n : 
+        #         return o2n[root]
+            
+        #     cp = Node(root.val)
+        #     o2n[root] = cp
+
+        #     for neighbor in root.neighbors : 
+        #         cp.neighbors.append(copy(neighbor))
+            
+        #     return cp 
         
-        dfs(start)
-        
-        for o , n in o_to_n.items() :
-            for nei in o.neighbors : 
-                n.neighbors.append(o_to_n[nei]) 
-        
-        return o_to_n[start]
+        build(node)
+
+        for old , n in o2n.items() : 
+            for neighbor in old.neighbors : 
+                n.neighbors.append(o2n[neighbor])
+
+
+        return o2n[node]
