aba634e8c3bdd5cc78bd5b140636e1aa285067b3
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-03-07 13:34:23 +0300

    Time: 278 ms (16.11%), Space: 109.1 MB (8.43%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,18 +1,24 @@
+
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        rows , cols = len(matrix) , len(matrix[0])
+        rows, cols = len(matrix) , len(matrix[0])
+        def in_range(r, c) : 
+            return 0 <= r < rows and 0 <= c < cols 
         @cache
         def explore(row , col) : 
-            if not (0 <= row < rows) or not (0 <= col < cols) or matrix[row][col] == "0" : 
-                return 0 
+            if not in_range(row , col) or matrix[row][col] == '0' : 
+                return 0
             
-            return 1 + min(explore(row-1 , col) , explore(row , col-1) , explore(row-1 , col-1))
+            return 1 + min(
+                explore(row-1 , col) , 
+                explore(row , col-1) , 
+                explore(row-1 , col-1) , 
+                )
         
         max_side = 0 
-
         for r in range(rows) : 
             for c in range(cols) : 
-                if matrix[r][c] == "1" : 
+                if matrix[r][c] == '1' : 
                     max_side = max(max_side , explore(r , c))
         
-        return max_side ** 2 
+        return max_side * max_side 
