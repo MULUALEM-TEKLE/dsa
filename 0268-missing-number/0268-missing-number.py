180c9e63677f253b17bac380cd4ebd2c0852e4f7
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-03-08 12:08:16 +0300

    Time: 1384 ms (12.5%), Space: 20.4 MB (93.79%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n_xor = 0
-
         for i in range(len(nums)+1) : 
-            n_xor = i ^ n_xor
-
-        nums_xor = n_xor
-        for num in nums : 
-            nums_xor ^= num
-        
-        return nums_xor
+            if i not in nums : return i
