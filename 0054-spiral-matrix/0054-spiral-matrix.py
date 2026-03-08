44dc3553c5df4b8af51e916ba6baa73969d71533
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-03-08 15:12:46 +0300

    Time: 0 ms (100%), Space: 19.3 MB (92.53%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -18,7 +18,7 @@ class Solution:
                 res.append(matrix[bottom-1][i])
             bottom -= 1 
 
-            for i in range(bottom-1 , top , -1) : 
+            for i in range(bottom-1 , top-1 , -1) : 
                 res.append(matrix[i][left])
             left += 1
         print(res)
