2c4b151b7cb211cc2f4e94f702dff84bd1bb4f14
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-03-09 18:49:19 +0300

    Time: 7 ms (30.55%), Space: 19.4 MB (35.25%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -9,7 +9,7 @@ class Solution:
 
         def check_and_add(value , seen_set) : 
             if value == '.' : 
-                    return True
+                return True
             else : 
                 if value in seen : return False
                 seen.add(value)
@@ -19,31 +19,18 @@ class Solution:
             seen = set()
             for c in range(cols) : 
                 value = board[r][c]
-                # if value == '.' : 
-                #     continue
-                # else : 
-                #     if value in seen : return False
-                #     seen.add(value)
                 if not check_and_add(value , seen) : return False 
         
         for c in range(cols) : 
             seen = set()
             for r in range(rows) : 
                 value = board[r][c]
-                if value == '.' : 
-                    continue
-                else : 
-                    if value in seen : return False
-                    seen.add(value)
+                if not check_and_add(value , seen) : return False 
         
         for i , j in box_starts : 
             seen = set()
             for r in range(i , i+3) : 
                 for c in range(j , j+3) : 
                     value = board[r][c] 
-                    if value == "." : 
-                        continue 
-                    else : 
-                        if value in seen : return False
-                        seen.add(value)
+                    if not check_and_add(value , seen) : return False 
         return True 
