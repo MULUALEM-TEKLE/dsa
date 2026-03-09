bab9a87e55d7d038e37a0a05f9dbc9f75c7242f3
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-03-09 18:46:09 +0300

    Time: 0 ms (100%), Space: 19.3 MB (93.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,39 +1,40 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        N = 9 
-
-        for r in range(N) : 
-            s = set()
-            for c in range(N) : 
-                cell = board[r][c]
-                if cell == "." : 
-                    continue
-                if cell in s : return False
-                s.add(cell)
-        
-        for c in range(N) : 
-            s = set()
-            for r in range(N) : 
-                cell = board[r][c]
-                if cell == "." : 
-                    continue
-                if cell in s : return False
-                s.add(cell)
-        
-        start = [
+        rows = cols = 9 
+        box_starts = [
             [0 , 0] , [0 , 3] , [0 , 6] , 
             [3 , 0] , [3 , 3] , [3 , 6] , 
             [6 , 0] , [6 , 3] , [6 , 6] , 
-
         ]
 
-        for r , c in start : 
-            s = set()
-            for row in range(r , r+3) : 
-                for col in range(c , c+3) : 
-                    cell = board[row][col]
-                    if cell == "." : continue 
-                    if cell in s : return False 
-                    s.add(cell)
-
-        return True
+        for r in range(rows) : 
+            seen = set()
+            for c in range(cols) : 
+                value = board[r][c]
+                if value == '.' : 
+                    continue
+                else : 
+                    if value in seen : return False
+                    seen.add(value)
+        
+        for c in range(cols) : 
+            seen = set()
+            for r in range(rows) : 
+                value = board[r][c]
+                if value == '.' : 
+                    continue
+                else : 
+                    if value in seen : return False
+                    seen.add(value)
+        
+        for i , j in box_starts : 
+            seen = set()
+            for r in range(i , i+3) : 
+                for c in range(j , j+3) : 
+                    value = board[r][c] 
+                    if value == "." : 
+                        continue 
+                    else : 
+                        if value in seen : return False
+                        seen.add(value)
+        return True 
