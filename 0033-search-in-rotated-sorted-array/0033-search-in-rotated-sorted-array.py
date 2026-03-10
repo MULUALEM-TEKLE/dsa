dd18b195ec1cac5fa7ffee1b070693a38e373712
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-03-10 07:57:56 +0300

    Time: 0 ms (100%), Space: 19.5 MB (73.42%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -8,9 +8,9 @@ class Solution:
 
             if nums[mid] > nums[right] : 
                 pivot = mid + 1
-                right = mid - 1
-            else : 
                 left = mid + 1
+            else : 
+                right = mid 
         
         print(pivot)
         
