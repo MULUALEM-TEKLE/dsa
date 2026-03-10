3d2731f11aa999a5eb8d322196fb636c173c13e0
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-03-10 12:41:36 +0300

    Time: 635 ms (69.03%), Space: 163.6 MB (14.45%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,31 +4,14 @@ class Solution:
         if total % 2 != 0 : return False
         target = total // 2
 
-        # dp = set()
-        # dp.add(0)
-
-        # for i in range(len(nums)-1, -1 , -1) : 
-        #     nextdp = set()
-        #     for t in dp :
-        #         summ = nums[i]+t
-        #         if summ == target :
-        #             return True
-        #         nextdp.add(t)
-        #         if summ > target : continue
-        #         nextdp.add(summ)
-        #     dp = nextdp
-
-        # return target in dp
-
-
         @cache
         def explore(i , cur) : 
             if cur > target : 
                 return False
-            if cur == target : 
-                return True 
+            # if cur == target : 
+            #     return True 
             if i == len(nums) : 
-                return False
+                return True if cur == target else False
             
             return explore(i+1 , cur+nums[i]) or explore(i+1 , cur)
         
