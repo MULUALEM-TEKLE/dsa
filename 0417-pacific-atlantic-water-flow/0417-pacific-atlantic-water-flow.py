30f8e9da2ea3b882afdd01ebea2c2d0aa515c2ec
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-03-19 11:15:52 +0300

    Time: 33 ms (43.25%), Space: 22.3 MB (44.89%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,38 +2,28 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         if not heights : return []
         rows , cols = len(heights) , len(heights[0])
-        pacific_initials = []
-        atlantic_initials = []
+        pacific_reachable = set()
+        atlantic_reachable = set()
 
-        for i in range(rows) : 
-            pacific_initials.append((i , 0))
-            atlantic_initials.append((i , cols-1))
-        for i in range(cols) : 
-            pacific_initials.append((0 , i))
-            atlantic_initials.append((rows-1 , i))
-
-        visited = set()
-        def dfs(row , col) : 
-            if not 0 <= row < rows or not 0 <= col < cols : 
-                return 
-            
-            visited.add((row , col))
+        def dfs(row , col , reachable_set) : 
+            reachable_set.add((row , col))
             for dr , dc in [[1 , 0] , [-1 , 0] , [0 , 1] , [0 , -1]] : 
                 nr , nc = row+dr , col+dc
-                if (nr , nc) not in visited and 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[row][col] : 
-                    visited.add((nr ,nc))
-                    dfs(nr , nc)
-        # print(atlantic_initials)
-        # print(pacific_initials)
-        for row , col in atlantic_initials : 
-            dfs(row , col)
-        from_atlantic = visited 
-        visited = set()
-        for row , col in pacific_initials : 
-            dfs(row , col)
-        from_pacific = visited
+                if (nr , nc) not in reachable_set and 0 <= nr < rows and 0 <= nc < cols : 
+                    if heights[nr][nc] >= heights[row][col] : dfs(nr , nc , reachable_set)
 
-        return list(from_atlantic & from_pacific)
+        for i in range(rows) : 
+            # pacific_initials.append((i , 0))
+            dfs(i , 0 , pacific_reachable)
+            # atlantic_initials.append((i , cols-1))
+            dfs(i , cols-1 , atlantic_reachable)
+        for i in range(cols) : 
+            # pacific_initials.append((0 , i))
+            dfs(0 , i , pacific_reachable)
+            # atlantic_initials.append((rows-1 , i))
+            dfs(rows-1 , i , atlantic_reachable)    
+
+        return list(pacific_reachable & atlantic_reachable)
             
 
         
