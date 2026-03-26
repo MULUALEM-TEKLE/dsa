8598cd81e16a8b5c7a0ad7a47d715f32ea6fb2ce
Mulualem Tekle <38713279+MULUALEM-TEKLE@users.noreply.github.com>
2026-03-26 10:15:16 +0300

    Time: 7 ms (79.28%), Space: 19.4 MB (87.65%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,46 +1,15 @@
-class TrieNode : 
-    def __init__(self) : 
-        self.children = {}
-        self.eow = False 
-        self.value = ""
-
-class Trie : 
-    def __init__(self) :
-        self.root = TrieNode()
-
-    def insert(self , word) : 
-        cur = self.root 
-
-        for c in word : 
-            if c not in cur.children : 
-                cur.children[c] = TrieNode()
-            cur = cur.children[c]
-        cur.eow = True 
-        cur.val = word 
-    
-    def bfs(self) : 
-        q = deque()
-        q.append(self.root)
-        res = ""
-
-        while q : 
-            node = q.popleft()
-
-            for child in node.children.values() : 
-                if child.eow : 
-                    if len(child.val) > len(res) : 
-                        res = child.val
-                    q.append(child)
-        
-        return res
-        
-
-
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        t = Trie()
+        safe = {""}
+        longest = ""
+
         words.sort()
+
         for word in words : 
-            t.insert(word)
+            if word[:-1] in safe : 
+                safe.add(word)
+            
+                if len(word) > len(longest) : 
+                    longest = word
         
-        return t.bfs()
+        return longest 
